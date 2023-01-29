0b1c58d930e9fac5c1eabe696151750912a8a417
James Tirta Halim <tirtajames45@gmail.com>
2023-01-29 19:42:40 +0700

    newest

--- str.c
+++ str.c
@@ -1,5 +1,8 @@
 #include <stdarg.h>
 #include <stdlib.h>
+#include <stdio.h>
+
+#define MAX_MALLOC 1024
 
 /* Put "\0" (with quotes) as the last parameter
 int addStr(char **outStr, ..., "\0") */
@@ -8,7 +11,7 @@ int joinStr(char **outStr, ...)
 	char *tmp;
 	va_list argp;
 	va_start(argp, outStr);
-	tmp = malloc(100);
+	tmp = malloc(MAX_MALLOC);
 	if (!tmp)
 		goto ERR;
 	int argLen=0;
@@ -27,12 +30,46 @@ int joinStr(char **outStr, ...)
 	va_end(argp);
 	return argLen;
 ERR:
+	fprintf(stderr, "joinStr:(char **outStr) ...");
+	perror("");
 	free(tmp);
 	va_end(argp);
 	return 0;
 }
 
-int main(void)
+int joinStrLarge(char **outStr, ...)
 {
+	char *tmp;
+	va_list argp;
+	va_start(argp, outStr);
+	tmp = malloc(MAX_MALLOC);
+	if (!tmp)
+		goto ERR;
+	int argLen=0;
+	for (;;) {
+		char *strArgv = va_arg(argp, char*);
+		if (!strArgv[0])
+			break;
+		for (int i=0 ; strArgv[i]; ++argLen, ++i)
+			tmp[argLen] = strArgv[i];
+		if (MAX_MALLOC < argLen * 4) {
+			char* tmp2 = realloc(tmp, (argLen * 8));
+			if (!tmp2)
+				goto ERR;
+			tmp = tmp2;
+		}
+	}
+	if (!argLen)
+		goto ERR;
+	*outStr = realloc(tmp, argLen);
+	if (!*outStr)
+		goto ERR;
+	va_end(argp);
+	return argLen;
+ERR:
+	fprintf(stderr, "joinStrLarge:(char **outStr) ...");
+	perror("");
+	free(tmp);
+	va_end(argp);
 	return 0;
 }
--- str.h
+++ str.h
@@ -1,2 +1,3 @@
 /* int joinStr(char **outStr, ..., "\0") */
 int joinStr(char **outStr, ...);
+int joinStrLarge(char **outStr, ...);
