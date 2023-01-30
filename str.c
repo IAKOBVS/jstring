9721f9300753ab156c2367a4d3c342ec95ca1c47
James Tirta Halim <tirtajames45@gmail.com>
2023-01-30 15:23:30 +0700

    newest

--- str.c
+++ str.c
@@ -2,48 +2,86 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-
 #include <string.h>
 
 #include "str.h"
 
 /* Put "\0" (with quotes) as the last parameter
-int joinStr(char **outStr, ..., "\0") */
-int joinStr(char **outStr, ...)
+int joinStr(char **dest, ..., "\0") */
+int addStrings(char **dest, ...)
 {
-	/* *outStr must be freed */
-	va_list argp;
-	va_start(argp, outStr);
 	int argLen=0;
+	va_list argp;
+	va_start(argp, dest);
 	for (;;) {
 		char *strArgv = va_arg(argp, char*);
 		if (!strArgv[0])
 			break;
 		argLen += strlen(strArgv);
 	}
+	int j;
 	va_end(argp);
 	if (!argLen)
 		goto ERR;
-	*outStr = malloc(++argLen);
-	if (!*outStr)
-		goto ERR;
-	va_start(argp, outStr);
-	int j=0;
+	if (!*dest) {
+		j=0;
+		*dest = malloc(++argLen);
+		if (!*dest)
+			goto ERR;
+	} else {
+		argLen += strlen(*dest);
+		j=argLen;
+		char *tmp = *dest;
+		*dest = malloc(++argLen);
+		if (!*dest)
+			goto ERR;
+		strcpy(*dest, tmp);
+	}
+	va_start(argp, dest);
 	for (;;) {
 		char *strArgv = va_arg(argp, char*);
 		if (!strArgv[0])
 			break;
 		int i=0;
 		do {
-			(*outStr)[j++] = strArgv[i++];
+			(*dest)[j++] = strArgv[i++];
 		} while (strArgv[i]);
 	}
-	(*outStr)[argLen] = '\0';
+	(*dest)[argLen] = '\0';
 	va_end(argp);
 	return argLen;
 
 ERR:
-	fprintf(stderr, "joinStr(char **outStr) ...):");
+	fprintf(stderr, "joinStr(char **dest) ...):");
+	perror("");
+	return 0;
+}
+
+int addStr(int destLen, char **dest, int srcLen, char *src)
+{
+	if (!destLen) {
+		destLen = strlen(*dest);
+		if (!destLen)
+			goto ERR;
+	}
+	if (!srcLen) {
+		srcLen = strlen(src);
+		if (!srcLen)
+			goto ERR;
+	}
+	srcLen += destLen + 1;
+	char *tmp = *dest;
+	*dest = malloc(srcLen);
+	if (!*dest)
+		goto ERR;
+	strcpy(*dest, tmp);
+	int i=0;
+	do {
+		(*dest)[destLen++] = src[i++];
+	} while (src[i]);
+	return srcLen;
+ERR:;
+	fprintf(stderr, "joinStr(char **dest) ...):");
 	perror("");
 	return 0;
 }
