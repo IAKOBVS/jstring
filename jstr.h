c7e51652d3b503d6299f9bc03a87c4e23b4a5f16
James Tirta Halim <tirtajames45@gmail.com>
2023-02-18 13:24:29 +0700

    newest

--- jstr.h
+++ jstr.h
@@ -4,6 +4,14 @@
 #include <stdlib.h>
 #include "/home/james/c/vargc.h"
 
+#if (defined(__GNUC__) && (__GNUC__ >= 3)) || (defined(__clang__) && __has_builtin(__builtin_expect))
+  #define likely(x) __builtin_expect(!!(x), 1)
+  #define unlikely(x) __builtin_expect(!!(x), 0)
+#else
+  #define likely(x) (x)
+  #define unlikely(x) (x)
+#endif
+
 #define JSTR_MIN_SIZE 8
 #define MAX(a,b) ((a)>(b)?(a):(b))
 
