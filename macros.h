7b709499ecb569daf0797183332482a238f1d2fa
James Tirta Halim <tirtajames45@gmail.com>
2023-03-25 10:43:32 +0700

    newest

--- macros.h
+++ macros.h
@@ -2,87 +2,87 @@
 #define JSTR_MACROS_H_DEF
 
 #if defined(__STDC_VERSION__) && __STDC_VERSION__ >= 199901L
-	#define RESTRICT restrict
+#	define RESTRICT restrict
 #elif defined(__GNUC__) || defined(__clang__)
-	#define RESTRICT __restrict__
+#	define RESTRICT __restrict__
 #elif defined(_MSC_VER)
-	#define RESTRICT __restrict
+#	define RESTRICT __restrict
 #else
-	#define RESTRICT
+#	define RESTRICT
 #endif // restrict
 
 #if defined(__GNUC__) || defined(__clang__)
-	#define ALWAYS_INLINE __attribute__((always_inline)) inline
+#	define ALWAYS_INLINE __attribute__((always_inline)) inline
 #elif defined(_MSC_VER)
-	#define ALWAYS_INLINE __forceinline inline
+#	define ALWAYS_INLINE __forceinline inline
 #else
-	#define ALWAYS_INLINE inline
+#	define ALWAYS_INLINE inline
 #endif // inline
 
 #if defined(__PRETTY_FUNCTION__)
-	#define CURR_FUNC __PRETTY_FUNCTION__
+#	define CURR_FUNC __PRETTY_FUNCTION__
 #elif defined(__FUNCTION__)
-	#define CURR_FUNC __FUNCTION__
+#	define CURR_FUNC __FUNCTION__
 #elif defined(__func__)
-	#define CURR_FUNC __func__
+#	define CURR_FUNC __func__
 #else
-	#define CURR_FUNC ""
+#	define CURR_FUNC ""
 #endif // __func__
 
 #if (defined(__GNUC__) && (__GNUC__ >= 3)) || (defined(__clang__) && __has_builtin(__builtin_expect))
-	#define likely(x) __builtin_expect(!!(x), 1)
-	#define unlikely(x) __builtin_expect(!!(x), 0)
+#	define likely(x) __builtin_expect(!!(x), 1)
+#	define unlikely(x) __builtin_expect(!!(x), 0)
 #else
-	#define likely(x) (x)
-	#define unlikely(x) (x)
+#	define likely(x) (x)
+#	define unlikely(x) (x)
 #endif // __has_builtin(__builtin_expect)
 
 #if defined(__GNUC__) || defined(__clang__)
-	#if __has_attribute(pure)
-		#define PURE __attribute__((pure))
-	#else
-		#define PURE
-	#endif
+#	if __has_attribute(pure)
+#		define PURE __attribute__((pure))
+#	else
+#		define PURE
+#	endif
 
-	#if __has_attribute(const)
-		#define CONST __attribute__((const))
-	#else
-		#define CONST
-	#endif
+#	if __has_attribute(const)
+#		define CONST __attribute__((const))
+#	else
+#		define CONST
+#	endif
 
-	#if __has_attribute(flatten)
-		#define FLATTEN __attribute__((flatten))
-	#else
-		#define FLATTEN
-	#endif
+#	if __has_attribute(flatten)
+#		define FLATTEN __attribute__((flatten))
+#	else
+#		define FLATTEN
+#	endif
 #elif defined(_MSC_VER)
-	#define PURE __declspec(noalias)
-	#define CONST __declspec(restrict)
-	#define FLATTEN
+#	define PURE __declspec(noalias)
+#	define CONST __declspec(restrict)
+#	define FLATTEN
 #else
-	#define PURE
-	#define CONST
-	#define FLATTEN
+#	define PURE
+#	define CONST
+#	define FLATTEN
 #endif // __GNUC__ || __clang__ || _MSC_VER
 
 #if defined(__GNUC__) || defined(__clang__)
-	#include <stdint.h>
-	#if __has_builtin(__builtin_clzll)
+#	include <stdint.h>
+#	if __has_builtin(__builtin_clzll)
 		CONST ALWAYS_INLINE static uint64_t private_jstr_next_pow2_64(uint64_t x)
 		{
 			return 1ull << (64 - __builtin_clzll(x - 1));
 		}
-	#endif // __has_builtin(__builtin_clzll)
-	#if __has_builtin(__builtin_clz)
+#	endif // __has_builtin(__builtin_clzll)
+#	if __has_builtin(__builtin_clz)
 		CONST ALWAYS_INLINE static uint32_t private_jstr_next_pow2_32(uint32_t x)
 		{
 			return 1 << (32 - __builtin_clz(x - 1));
 		}
-	#endif // __has_builtin(__builtin_clz)
+#	endif // __has_builtin(__builtin_clz)
 #elif defined(_MSC_VER)
-	#include <stdint.h>
-	#include <intrin.h>
-	#pragma intrinsic(_BitScanReverse64)
+#	include <stdint.h>
+#	include <intrin.h>
+#	pragma intrinsic(_BitScanReverse64)
 	CONST ALWAYS_INLINE static uint32_t private_jstr_next_pow2_32(uint32_t x)
 	{
 		unsigned long index;
