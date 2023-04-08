e8ccf286b9a04947483ed95320fc935cd04664b5
James Tirta Halim <tirtajames45@gmail.com>
2023-04-08 11:48:43 +0700

    newest

---
+++ jstrio.h
@@ -0,0 +1,35 @@
+#include "macros.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stddef.h>
+#include <stdio.h>
+#include <sys/stat.h>
+
+JSTR_INLINE__
+JSTR_WARN_UNUSED__
+size_t jstrio_sizeof_file(const char *JSTR_RESTRICT__ const filename) JSTR_NOEXCEPT__
+{
+	struct stat st;
+	return (!stat(filename, &st) ? st.st_size : 0);
+}
+
+JSTR_INLINE__
+JSTR_WARN_UNUSED__
+int jstrio_readfile(char *JSTR_RESTRICT__ buf,
+		const char *JSTR_RESTRICT__ filename,
+		const size_t sizeof_file) JSTR_NOEXCEPT__
+{
+	FILE *fp = fopen(filename, "r");
+	if (unlikely(!fp))
+		return 0;
+	fread(JSTR_CAST__(void *)buf, 1, sizeof_file, fp);
+	fclose(fp);
+	return 1;
+}
+
+#ifdef __cplusplus
+}
+#endif
