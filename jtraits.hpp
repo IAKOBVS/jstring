e0523e4c4ddd81c82dff26ff2bf88d7c68e659e0
James Tirta Halim <tirtajames45@gmail.com>
2023-04-18 08:19:55 +0700

    newest

--- jtraits.hpp
+++ jtraits.hpp
@@ -54,38 +54,38 @@ static constexpr void assert_are_strings(Str&&, StrArgs&&...) JSTR_NOEXCEPT__
 
 template <typename T, typename Arg>
 JSTR_INLINE__
-static constexpr int are_type() JSTR_NOEXCEPT__
+static constexpr int are_same_type() JSTR_NOEXCEPT__
 {
 	return std::is_same<T, std::decay_t<Arg>>::value;
 }
 
 template <typename T, typename Arg, typename... Args, typename = typename std::enable_if<sizeof...(Args) != 0>::type>
 JSTR_INLINE__
-static constexpr int are_type() JSTR_NOEXCEPT__
+static constexpr int are_same_type() JSTR_NOEXCEPT__
 {
-	return are_type<T, Arg>()
-	&& are_type<T, Args...>();
+	return are_same_type<T, Arg>()
+	&& are_same_type<T, Args...>();
 }
 
 template<typename T, typename Arg>
 JSTR_INLINE__
-static constexpr int are_type(Arg) JSTR_NOEXCEPT__
+static constexpr int are_same_type(Arg) JSTR_NOEXCEPT__
 {
-	return are_type<T, Arg>();
+	return are_same_type<T, Arg>();
 }
 
 template<typename T, typename Arg, typename... Args>
 JSTR_INLINE__
-static constexpr int are_type(Arg, Args...) JSTR_NOEXCEPT__
+static constexpr int are_same_type(Arg, Args...) JSTR_NOEXCEPT__
 {
-	return are_type<T, Arg, Args...>();
+	return are_same_type<T, Arg, Args...>();
 }
 
 template <typename T, typename Arg, typename... Args>
 JSTR_INLINE__
-static constexpr void assert_are_type(Arg&&, Args&&...) JSTR_NOEXCEPT__
+static constexpr void assert_are_same_type(Arg&&, Args&&...) JSTR_NOEXCEPT__
 {
-	static_assert(are_type<T, Arg, Args...>(), "Passing non-T as T argument!");
+	static_assert(are_same_type<T, Arg, Args...>(), "Passing non-T as T arguments!");
 }
 
 }
