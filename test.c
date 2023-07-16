dc03fbd2130ba89ee36b018da2e7ee0e236f9ea2
James Tirta Halim <tirtajames45@gmail.com>
2023-07-16 20:10:16 +0700

    newest

--- test.c
+++ test.c
@@ -6,6 +6,9 @@ int main()
 	jstd_declare_string(s, ssz, scap);
 	jstd_alloc_append(&s, &ssz, &scap, "wha1234567890123456789--_________________-1234567890123456789");
 	jstd_replaceall(&s, &ssz, &scap, "1234567890123456789", "");
+	printf("len:%zu\n", strlen(s));
+	printf("sz:%zu\n", ssz);
+	printf("cap:%zu\n", scap);
 	puts(s);
 	free(s);
 	return 0;
