77cd34aad3ebf94ba05a7fc2921913d7770146ff
James Tirta Halim <tirtajames45@gmail.com>
2023-07-18 19:10:04 +0700

    newest

--- test.c
+++ test.c
@@ -1,15 +1,15 @@
 #define _GNU_SOURCE
-#include "jstd.h"
+#include "jstr.h"
 
 int main()
 {
-	jstd_declare_string(s, ssz, scap);
-	jstd_alloc_append(&s, &ssz, &scap, "wha1234567890123456789--_________________-1234567890123456789");
-	jstd_replaceall(&s, &ssz, &scap, "1234567890123456789", "");
-	printf("len:%zu\n", strlen(s));
-	printf("sz:%zu\n", ssz);
-	printf("cap:%zu\n", scap);
-	puts(s);
-	free(s);
+	jstr_t s;
+	jstr_alloc_append(&s.data, &s.size, &s.cap, "wha1234567890123456789--_________________-1234567890123456789");
+	jstr_replaceall(&s.data, &s.size, &s.cap, "1234567890123456789", "");
+	printf("len:%zu\n", strlen(s.data));
+	printf("sz:%zu\n", s.size);
+	printf("cap:%zu\n", s.cap);
+	puts(s.data);
+	free(s.data);
 	return 0;
 }
