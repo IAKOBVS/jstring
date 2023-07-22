3f1ed9995c3ada7f8d841712965de4e084a02e08
James Tirta Halim <tirtajames45@gmail.com>
2023-07-22 15:19:36 +0700

    newest

--- gen_func.pl
+++ gen_func.pl
@@ -2,10 +2,10 @@
 use strict;
 use warnings;
 
-my $NAMESPACE     = 'jstr';
+my $NAMESPACE 	= 'jstr';
 my $NAMESPACE_BIG = 'JSTR';
-my $DIR_CPP       = 'cpp';
-my $DIR_C         = 'c';
+my $DIR_CPP   	= 'cpp';
+my $DIR_C 		= 'c';
 
 mkdir($DIR_CPP);
 mkdir($DIR_C);
@@ -17,24 +17,27 @@ if ($#ARGV != 0) {
 }
 my $file;
 my $namespace = '';
-my $undef     = '';
-my $endif     = '';
-open(my $FH, '<', $FNAME)
-  or die "Can't open $FNAME\n";
+my $undef 	= '';
+my $endif 	= '';
+open(my $FH, '<', $FNAME) or die "Can't open $FNAME\n";
 while (<$FH>) {
 	if (/#\s*undef/) {
-	    $undef .= $_;
+		$undef .= $_;
 	} elsif (/#\s*endif\s*(?:\/\/|\/\*).*DEF.*(?:\/\*){0,1}/) {
-	    $endif .= $_;
+		$endif .= $_;
 	} elsif (/^\s*#if\s*defined.*JSTR_EXTERN_C/) {
-	    $namespace .= $_;
-	    while (<$FH>) {
-	        last if /^$/;
-	        $namespace .= $_;
-	    }
-	    $namespace = '' if $namespace =~ /namespace/;
+		$namespace .= $_;
+		while (<$FH>) {
+			if (/^$/) {
+				last;
+			}
+			$namespace .= $_;
+		}
+		if ($namespace =~ /namespace/) {
+			$namespace = '';
+		}
 	} else {
-	    $file .= $_;
+		$file .= $_;
 	}
 }
 close($FH);
@@ -43,10 +46,12 @@ my $hpp;
 my @funcs = split(/\n\n/, $file);
 foreach (@funcs) {
 	/^((?:\/\/|\/\*|$NAMESPACE_BIG|static)[^(){}]+?($NAMESPACE\_\w+?)\(((?:.|\n)+?(?:sz|cap)(?:.|\n)+?\)\s*\w*NOEXCEPT))/;
-	next if !$1 || !$2 || !$3;
-	my $decl    = $1;
-	my $func    = $2;
-	my $args    = $3;
+	if (!$1 || !$2 || !$3) {
+		next;
+	}
+	my $decl	= $1;
+	my $func	= $2;
+	my $args	= $3;
 	my $has_sz  = (index($args, 'sz') != -1)  ? 1 : 0;
 	my $has_cap = (index($args, 'cap') != -1) ? 1 : 0;
 	next if !$has_sz && !$has_cap;
@@ -54,62 +59,66 @@ foreach (@funcs) {
 	my $is_const;
 	my @new_args;
 	{
-	    my @old_args = split(/\s/, $args);
-	    my $i        = 0;
-	    foreach (@old_args) {
-	        if ($i == 0) {
-	            if ($_ =~ /const/) {
-	                $is_const = 1;
-	            } else {
-	                $is_const = 0;
-	            }
-	        }
-	        if (/,$/) {
-	            s/,//;
-	            push(@new_args, $_);
-	            ++$i;
-	        }
-	    }
-	    for (my $i = 0 ; $i < $#old_args ; ++$i) {
-	        if ($i == 0) {
-	            if ($_ =~ /const/) {
-	                $is_const = 1;
-	            } else {
-	                $is_const = 0;
-	            }
-	        }
-	        if (/,$/) {
-	            s/,//;
-	            push(@new_args, $_);
-	            ++$i;
-	        }
-	    }
+		my @old_args = split(/\s/, $args);
+		my $i		= 0;
+		foreach (@old_args) {
+			if ($i == 0) {
+				if ($_ =~ /const/) {
+					$is_const = 1;
+				} else {
+					$is_const = 0;
+				}
+			}
+			if (/,$/) {
+				s/,//;
+				push(@new_args, $_);
+				++$i;
+			}
+		}
+		for (my $i = 0 ; $i < $#old_args ; ++$i) {
+			if ($i == 0) {
+				if ($_ =~ /const/) {
+					$is_const = 1;
+				} else {
+					$is_const = 0;
+				}
+			}
+			if (/,$/) {
+				s/,//;
+				push(@new_args, $_);
+				++$i;
+			}
+		}
 	}
 	my $tmp = '';
 	if ($has_sz) {
-	    $decl =~ s/\((?:.|\n)+?sz(,|\))/(/;
-	    $tmp = $1 if $1;
+		$decl =~ s/\((?:.|\n)+?sz(,|\))/(/;
+		if ($1) {
+			$tmp = $1;
+		}
 	}
 	if ($has_cap) {
-	    $decl =~ s/\((?:.|\n)+?cap(,|\))/(/;
-	    $tmp = $1 if $1 eq ',';
+		$decl =~ s/\((?:.|\n)+?cap(,|\))/(/;
+		 if ($1 eq ',') {
+			$tmp = $1
+		}
 	}
 	if ($is_const) {
-	    $decl =~ s/\(/(const $NAMESPACE\_t *j$tmp/;
+		$decl =~ s/\(/(const $NAMESPACE\_t *j$tmp/;
 	} else {
-	    $decl =~ s/\(/($NAMESPACE\_t *j$tmp/;
+		$decl =~ s/\(/($NAMESPACE\_t *j$tmp/;
 	}
 	$decl .= "\n{\n\t$func(";
 	my $func_args;
 	for (my $i = 0 ; $i <= $#new_args ; ++$i) {
-	    if (index($new_args[$i], 'sz') != -1) {
-	        $new_args[$i] = "&j->size";
-	    } elsif (index($new_args[$i], 'cap') != -1) {
-	        $new_args[$i] = "&j->cap";
-	    } elsif ($i == 0) {
-	        $new_args[$i] = "&j->data";
-	    }
-	    $func_args .= "$new_args[$i], ";
+		if (index($new_args[$i], 'sz') != -1) {
+			$new_args[$i] = "&j->size";
+		} elsif (index($new_args[$i], 'cap') != -1) {
+			$new_args[$i] = "&j->cap";
+		} elsif ($i == 0) {
+			$new_args[$i] = "&j->data";
+		}
+		$func_args .= "$new_args[$i], ";
 	}
 	$func_args =~ s/,[^,]*$//;
 	$decl .= "$func_args);\n}\n\n";
@@ -137,11 +146,9 @@ $hpp =~ s/$NAMESPACE\_(\w+\()/$1/g;
 $hpp =~ s/alloc_append/alloc/g;
 $hpp =~ s/\n\n\n/\n\n/g;
 $h   =~ s/\n\n\n/\n\n/g;
-open($FH, '>', "$DIR_CPP/$FNAME" . 'pp')
-  or die "Can't open $DIR_CPP/$FNAME" . "pp\n";
+open($FH, '>', "$DIR_CPP/$FNAME" . 'pp') or die "Can't open $DIR_CPP/$FNAME" . "pp\n";
 print($FH $hpp);
 close($FH);
-open($FH, '>', "$DIR_C/$FNAME")
-  or die "Can't open $DIR_C/$FNAME\n";
+open($FH, '>', "$DIR_C/$FNAME") or die "Can't open $DIR_C/$FNAME\n";
 print($FH $h);
 close($FH);
