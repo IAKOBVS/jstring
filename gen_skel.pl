fd1d7cb4e837a11d1dd9be949da7870e61a02b2f
James Tirta Halim <tirtajames45@gmail.com>
2023-07-26 16:44:15 +0700

    newest

--- gen_skel.pl
+++ gen_skel.pl
@@ -1,11 +1,12 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+# use diagnostics;
 
 my $NAMESPACE     = 'jstr';
-my $NAMESPACE_BIG = 'JSTR';
-my $DIR_CPP       = 'cpp';
+my $NAMESPACE_BIG = uc($NAMESPACE);
 my $DIR_C         = 'c';
+my $DIR_CPP       = $DIR_C . 'pp';
 
 mkdir($DIR_CPP);
 mkdir($DIR_C);
@@ -15,8 +16,8 @@ if ($#ARGV != 0) {
 	print "Usage: ./gen_func.pl <file>";
 	exit 1;
 }
-
-open(my $FH, '<', $FNAME) or die "Can't open $FNAME\n";
+open(my $FH, '<', $FNAME)
+  or die "Can't open $FNAME\n";
 my $file;
 while (<$FH>) {
 	$file .= $_;
@@ -30,6 +31,7 @@ foreach (@funcs) {
 	}
 }
 $file =~ s/(.*&& $NAMESPACE_BIG\_EXTERN_C)/$skel$1/;
-open($FH, '>', $FNAME) or die "Can't open $FNAME\n";
+open($FH, '>', $FNAME)
+  or die "Can't open $FNAME\n";
 print $FH $file;
 close($FH);
