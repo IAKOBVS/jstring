07f98f03c1fcfd45e9fe6fe93da07d6dd7f63463
James Tirta Halim <tirtajames45@gmail.com>
2023-07-28 22:49:24 +0700

    newest

--- gen_func.pl
+++ gen_func.pl
@@ -9,7 +9,10 @@ my $DIR_C = 'c';
 
 update_needed();
 
-my $DIR_CPP       = $DIR_C . 'pp';
+my $DIR_CPP = $DIR_C . 'pp';
+my $OUT_C   = "$DIR_C/$FNAME";
+my $OUT_CPP = "$DIR_CPP/$FNAME" . 'pp';
+
 my $NAMESPACE     = 'jstr';
 my $NAMESPACE_BIG = uc($NAMESPACE);
 
@@ -29,34 +32,34 @@ my $RESTRICT_MACRO = $NAMESPACE_BIG . '_RST';
 mkdir($DIR_CPP);
 mkdir($DIR_C);
 
-my $file = get_file_buf();
-my $h    = '';
-my $hpp  = '';
+my $file    = get_file_buf();
+my $out_h   = '';
+my $out_hpp = '';
 
 my $RE_FUNC   = qr/^((?:\/\*|\/\/|$NAMESPACE_BIG\_|static)[^(){}]*($NAMESPACE\_\w*)\(([^\)]*\)[ \t]*\w*NOEXCEPT))/;
 my @NEW_LINES = gen_nonmem_funcs(split(/\n\n/, $file));
 gen_struct_funcs(@NEW_LINES);
-print_to_file("$DIR_C/$FNAME", "$DIR_CPP/$FNAME");
+print_to_file($OUT_C, $OUT_CPP);
 
 sub usage
 {
 	if ($#ARGV != 0) {
-		print 'Usage: ./gen_func.pl <file>';
-		exit 1;
+		print('Usage: ./gen_func.pl <file>');
+		exit(1);
 	}
 }
 
 sub update_needed
 {
 	if (system("test $FNAME -nt $DIR_C/$FNAME || test $0 -nt $DIR_C")) {
-		exit 1;
+		exit(1);
 	}
 }
 
 sub get_file_buf
 {
 	open(my $FH, '<', $FNAME)
-	  or die "Can't open $FNAME\n";
+	  or die("Can't open $FNAME\n");
 	my $file = '';
 	while (<$FH>) {
 		$file .= $_;
@@ -69,12 +72,12 @@ sub print_to_file
 {
 	my ($h, $hpp) = @_;
 	open(my $FH, '>', $h)
-	  or die "Can't open $h\n";
-	print($FH $h);
+	  or die("Can't open $out_h\n");
+	print($FH $out_h);
 	close($FH);
 	open($FH, '>', $hpp)
-	  or die "Can't open $hpp\n";
-	print($FH $hpp);
+	  or die("Can't open $out_hpp\n");
+	print($FH $out_hpp);
 	close($FH);
 }
 
@@ -215,27 +218,26 @@ sub gen_struct_funcs
 		$_    .= "\n\n";
 		$decl .= "\n\n";
 		$decl =~ s/$FUNC_NAME/$FUNC_NAME\_j/;
-		$hpp .= $_;
-		$hpp .= $decl;
-		$h   .= $_;
-		$h   .= $decl;
+		$out_hpp .= $_;
+		$out_hpp .= $decl;
+		$out_h   .= $_;
+		$out_h   .= $decl;
 		next;
 	  NEXT:
-		$_   .= "\n\n";
-		$h   .= $_;
-		$hpp .= $_;
+		$_       .= "\n\n";
+		$out_h   .= $_;
+		$out_hpp .= $_;
 	}
-	$hpp =~ s/\.h"/.hpp"/g;
-	$hpp =~ s/H_DEF/HPP_DEF/g;
-	$hpp =~ s/EXTERN_C\s*\d/EXTERN_C 0/;
-	$hpp =~ s/NAMESPACE\s*\d/NAMESPACE 1/;
+	$out_hpp =~ s/\.h"/.hpp"/g;
+	$out_hpp =~ s/H_DEF/HPP_DEF/g;
+	$out_hpp =~ s/EXTERN_C\s*\d/EXTERN_C 0/;
+	$out_hpp =~ s/NAMESPACE\s*\d/NAMESPACE 1/;
 
-	# $hpp =~ s/($NAMESPACE\_\w*)_mem(\w*\()/$1$2/g;
-	$hpp =~ s/$NAMESPACE\_(\w*\()/$1/g;
-	$hpp =~ s/\tt\(/\t$STR_STRUCT(/g;
-	$hpp =~ s/\t~t\(/\t$STR_STRUCT(/g;
-	$hpp =~ s/\n#if.*\s*#endif.*/\n/g;
-	$hpp =~ s/\n\n\n/\n\n/g;
-	$h   =~ s/\n\n\n/\n\n/g;
-
+	# $out_hpp =~ s/($NAMESPACE\_\w*)_mem(\w*\()/$1$2/g;
+	$out_hpp =~ s/$NAMESPACE\_(\w*\()/$1/g;
+	$out_hpp =~ s/\tt\(/\t$STR_STRUCT(/g;
+	$out_hpp =~ s/\t~t\(/\t$STR_STRUCT(/g;
+	$out_hpp =~ s/\n#if.*\s*#endif.*/\n/g;
+	$out_hpp =~ s/\n\n\n/\n\n/g;
+	$out_h   =~ s/\n\n\n/\n\n/g;
 }
