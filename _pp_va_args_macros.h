dbfec9e7d8a8ccdd92985f64619daac1e335303a
James Tirta Halim <tirtajames45@gmail.com>
2023-08-25 22:38:03 +0700

    newest

--- _pp_va_args_macros.h
+++ _pp_va_args_macros.h
@@ -12,133 +12,133 @@
 
 #ifndef PRIV_JSTR_PP_CASE_VOWEL_UPPER
 #	define PRIV_JSTR_PP_CASE_VOWEL_LOWER \
-	case 'a':                        \
-	case 'i':                        \
-	case 'u':                        \
-	case 'e':                        \
+	case 'a':                             \
+	case 'i':                             \
+	case 'u':                             \
+	case 'e':                             \
 	case 'o':
 #	define PRIV_JSTR_PP_CASE_VOWEL_UPPER \
-	case 'A':                        \
-	case 'I':                        \
-	case 'U':                        \
-	case 'E':                        \
+	case 'A':                             \
+	case 'I':                             \
+	case 'U':                             \
+	case 'E':                             \
 	case 'O':
-#	define PRIV_JSTR_PP_CASE_VOWEL \
-PRIV_JSTR_PP_CASE_VOWEL_UPPER:          \
+#	define PRIV_JSTR_PP_CASE_VOWEL       \
+		PRIV_JSTR_PP_CASE_VOWEL_UPPER \
 		PRIV_JSTR_PP_CASE_VOWEL_LOWER
 
 #	define PRIV_JSTR_PP_CASE_CONSONANT_LOWER \
-	case 'b':                            \
-	case 'c':                            \
-	case 'd':                            \
-	case 'f':                            \
-	case 'g':                            \
-	case 'h':                            \
-	case '_j':                            \
-	case 'k':                            \
-	case 'l':                            \
-	case 'm':                            \
-	case 'n':                            \
-	case 'p':                            \
-	case 'q':                            \
-	case 'r':                            \
-	case 's':                            \
-	case 't':                            \
-	case 'v':                            \
-	case 'w':                            \
-	case 'x':                            \
-	case 'y':                            \
+	case 'b':                                 \
+	case 'c':                                 \
+	case 'd':                                 \
+	case 'f':                                 \
+	case 'g':                                 \
+	case 'h':                                 \
+	case 'j':                                 \
+	case 'k':                                 \
+	case 'l':                                 \
+	case 'm':                                 \
+	case 'n':                                 \
+	case 'p':                                 \
+	case 'q':                                 \
+	case 'r':                                 \
+	case 's':                                 \
+	case 't':                                 \
+	case 'v':                                 \
+	case 'w':                                 \
+	case 'x':                                 \
+	case 'y':                                 \
 	case 'z':
 #	define PRIV_JSTR_PP_CASE_CONSONANT_UPPER \
-	case 'B':                            \
-	case 'C':                            \
-	case 'D':                            \
-	case 'F':                            \
-	case 'G':                            \
-	case 'H':                            \
-	case 'J':                            \
-	case 'K':                            \
-	case 'L':                            \
-	case 'M':                            \
-	case 'N':                            \
-	case 'P':                            \
-	case 'Q':                            \
-	case 'R':                            \
-	case 'S':                            \
-	case 'T':                            \
-	case 'V':                            \
-	case 'W':                            \
-	case 'X':                            \
-	case 'Y':                            \
+	case 'B':                                 \
+	case 'C':                                 \
+	case 'D':                                 \
+	case 'F':                                 \
+	case 'G':                                 \
+	case 'H':                                 \
+	case 'J':                                 \
+	case 'K':                                 \
+	case 'L':                                 \
+	case 'M':                                 \
+	case 'N':                                 \
+	case 'P':                                 \
+	case 'Q':                                 \
+	case 'R':                                 \
+	case 'S':                                 \
+	case 'T':                                 \
+	case 'V':                                 \
+	case 'W':                                 \
+	case 'X':                                 \
+	case 'Y':                                 \
 	case 'Z':
-#	define PRIV_JSTR_PP_CASE_CONSONANT \
-PRIV_JSTR_PP_CASE_CONSONANT_UPPER:          \
+#	define PRIV_JSTR_PP_CASE_CONSONANT       \
+		PRIV_JSTR_PP_CASE_CONSONANT_UPPER \
 		PRIV_JSTR_PP_CASE_CONSONANT_LOWER
 
 #	define PRIV_JSTR_PP_CASE_DIGIT \
-	case '0':                  \
-	case '1':                  \
-	case '2':                  \
-	case '3':                  \
-	case '4':                  \
-	case '5':                  \
-	case '6':                  \
-	case '7':                  \
-	case '8':                  \
+	case '0':                       \
+	case '1':                       \
+	case '2':                       \
+	case '3':                       \
+	case '4':                       \
+	case '5':                       \
+	case '6':                       \
+	case '7':                       \
+	case '8':                       \
 	case '9':
 #	define PRIV_JSTR_PP_CASE_LOWER \
-	case 'a':                  \
-	case 'b':                  \
-	case 'c':                  \
-	case 'd':                  \
-	case 'e':                  \
-	case 'f':                  \
-	case 'g':                  \
-	case 'h':                  \
-	case 'i':                  \
-	case '_j':                  \
-	case 'k':                  \
-	case 'l':                  \
-	case 'm':                  \
-	case 'n':                  \
-	case 'o':                  \
-	case 'p':                  \
-	case 'q':                  \
-	case 'r':                  \
-	case 's':                  \
-	case 't':                  \
-	case 'u':                  \
-	case 'v':                  \
-	case 'w':                  \
-	case 'x':                  \
-	case 'y':                  \
+	case 'a':                       \
+	case 'b':                       \
+	case 'c':                       \
+	case 'd':                       \
+	case 'e':                       \
+	case 'f':                       \
+	case 'g':                       \
+	case 'h':                       \
+	case 'i':                       \
+	case 'j':                       \
+	case 'k':                       \
+	case 'l':                       \
+	case 'm':                       \
+	case 'n':                       \
+	case 'o':                       \
+	case 'p':                       \
+	case 'q':                       \
+	case 'r':                       \
+	case 's':                       \
+	case 't':                       \
+	case 'u':                       \
+	case 'v':                       \
+	case 'w':                       \
+	case 'x':                       \
+	case 'y':                       \
 	case 'z':
 #	define PRIV_JSTR_PP_CASE_UPPER \
-	case 'A':                  \
-	case 'B':                  \
-	case 'C':                  \
-	case 'D':                  \
-	case 'E':                  \
-	case 'F':                  \
-	case 'G':                  \
-	case 'H':                  \
-	case 'I':                  \
-	case 'J':                  \
-	case 'K':                  \
-	case 'L':                  \
-	case 'M':                  \
-	case 'N':                  \
-	case 'O':                  \
-	case 'P':                  \
-	case 'Q':                  \
-	case 'R':                  \
-	case 'S':                  \
-	case 'T':                  \
-	case 'U':                  \
-	case 'V':                  \
-	case 'W':                  \
-	case 'X':                  \
-	case 'Y':                  \
+	case 'A':                       \
+	case 'B':                       \
+	case 'C':                       \
+	case 'D':                       \
+	case 'E':                       \
+	case 'F':                       \
+	case 'G':                       \
+	case 'H':                       \
+	case 'I':                       \
+	case 'J':                       \
+	case 'K':                       \
+	case 'L':                       \
+	case 'M':                       \
+	case 'N':                       \
+	case 'O':                       \
+	case 'P':                       \
+	case 'Q':                       \
+	case 'R':                       \
+	case 'S':                       \
+	case 'T':                       \
+	case 'U':                       \
+	case 'V':                       \
+	case 'W':                       \
+	case 'X':                       \
+	case 'Y':                       \
 	case 'Z':
 #endif
 
@@ -152,67 +152,67 @@ PRIV_JSTR_PP_CASE_CONSONANT_UPPER:          \
 
 #ifndef __cplusplus
 #	define PRIV_JSTR_PP_PRINTF_FORMAT(x) _Generic((x),                       \
-						  unsigned char              \
-						  : "%hhu\n",                \
-						    unsigned char *          \
-						  : "%s\n",                  \
-						    char                     \
-						  : "%c\n",                  \
-						    char *                   \
-						  : "%s\n",                  \
-						    short                    \
-						  : "%hd\n",                 \
-						    unsigned short           \
-						  : "%hu\n",                 \
-						    unsigned int             \
-						  : "%u\n",                  \
-						    int                      \
-						  : "%d\n",                  \
-						    double                   \
-						  : "%f\n",                  \
-						    long double              \
-						  : "%Lf\n",                 \
-						    float                    \
-						  : "%f\n",                  \
-						    size_t                   \
-						  : "%zu\n",                 \
-						    long                     \
-						  : "%ld\n",                 \
-						    long long                \
-						  : "%lld\n",                \
-						    unsigned long long       \
-						  : "%llu\n",                \
-                                                                             \
-						    const unsigned char      \
-						  : "%hhu\n",                \
-						    const unsigned char *    \
-						  : "%s\n",                  \
-						    const char               \
-						  : "%c\n",                  \
-						    const char *             \
-						  : "%s\n",                  \
-						    const short              \
-						  : "%hd\n",                 \
-						    const unsigned short     \
-						  : "%hu\n",                 \
-						    const unsigned int       \
-						  : "%u\n",                  \
-						    const int                \
-						  : "%d\n",                  \
-						    const double             \
-						  : "%f\n",                  \
-						    const long double        \
-						  : "%Lf\n",                 \
-						    const float              \
-						  : "%f\n",                  \
-						    const size_t             \
-						  : "%zu\n",                 \
-						    const long               \
-						  : "%ld\n",                 \
-						    const long long          \
-						  : "%lld\n",                \
-						    const unsigned long long \
-						  : "%llu\n")
+						       unsigned char              \
+						       : "%hhu\n",                \
+							 unsigned char *          \
+						       : "%s\n",                  \
+							 char                     \
+						       : "%c\n",                  \
+							 char *                   \
+						       : "%s\n",                  \
+							 short                    \
+						       : "%hd\n",                 \
+							 unsigned short           \
+						       : "%hu\n",                 \
+							 unsigned int             \
+						       : "%u\n",                  \
+							 int                      \
+						       : "%d\n",                  \
+							 double                   \
+						       : "%f\n",                  \
+							 long double              \
+						       : "%Lf\n",                 \
+							 float                    \
+						       : "%f\n",                  \
+							 size_t                   \
+						       : "%zu\n",                 \
+							 long                     \
+						       : "%ld\n",                 \
+							 long long                \
+						       : "%lld\n",                \
+							 unsigned long long       \
+						       : "%llu\n",                \
+                                                                                  \
+							 const unsigned char      \
+						       : "%hhu\n",                \
+							 const unsigned char *    \
+						       : "%s\n",                  \
+							 const char               \
+						       : "%c\n",                  \
+							 const char *             \
+						       : "%s\n",                  \
+							 const short              \
+						       : "%hd\n",                 \
+							 const unsigned short     \
+						       : "%hu\n",                 \
+							 const unsigned int       \
+						       : "%u\n",                  \
+							 const int                \
+						       : "%d\n",                  \
+							 const double             \
+						       : "%f\n",                  \
+							 const long double        \
+						       : "%Lf\n",                 \
+							 const float              \
+						       : "%f\n",                  \
+							 const size_t             \
+						       : "%zu\n",                 \
+							 const long               \
+						       : "%ld\n",                 \
+							 const long long          \
+						       : "%lld\n",                \
+							 const unsigned long long \
+						       : "%llu\n")
 #	define jstr_pp_cout(x) printf(PRIV_JSTR_PP_PRINTF_FORMAT(x), x)
 #else
 #	include <iostream>
