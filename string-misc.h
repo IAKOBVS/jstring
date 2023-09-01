5ce2283363bff8905778efe64630b11ada733f74
James Tirta Halim <tirtajames45@gmail.com>
2023-09-01 17:28:32 +0700

    newest

--- string-misc.h
+++ string-misc.h
@@ -1,5 +1,5 @@
 #ifndef PJSTR_SELECTOR_STRING_MISC_H
-#define PJSTR_SELECTOR_STRING_MISC_H
+#define PJSTR_SELECTOR_STRING_MISC_H 1
 
 #include "generic-string-misc.h"
 #include "jstr_macros.h"
--- string-shift.h
+++ string-shift.h
@@ -1,7 +1,12 @@
 #ifndef PJSTR_SELECTOR_STRING_SHIFT_H
-#define PJSTR_SELECTOR_STRING_SHIFT_H
+#define PJSTR_SELECTOR_STRING_SHIFT_H 1
 
 #include "jstr_macros.h"
-#include "generic-string-shift.h"
+
+#if JSTR_ARCH_ALPHA
+#	include "alpha-string-shift.h"
+#else
+#	include "generic-string-shift.h"
+#endif
 
 #endif /* PJSTR_SELECTOR_STRING_SHIFT_H */
