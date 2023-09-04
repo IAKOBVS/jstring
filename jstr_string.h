df062a53f18bef9fdcc1e00e4898360fa95b8f16
James Tirta Halim <tirtajames45@gmail.com>
2023-09-05 00:14:08 +0700

    newest

--- jstr_string.h
+++ jstr_string.h
@@ -132,7 +132,7 @@ pjstr_strrstr_mem_bmh(const unsigned char *JSTR_RST _hs,
 		      const unsigned char *JSTR_RST const _ne,
 		      const size_t _nelen) JSTR_NOEXCEPT
 {
-#define PJSTR_HASH2(p) (((size_t)(p)[0] - ((size_t)(p)[-1] << 3)) % 256)
+#define H(p) (((size_t)(p)[0] - ((size_t)(p)[-1] << 3)) % 256)
 #define PJSTR_STRRSTR_BMH(table_type, ne_iterator_type)                           \
 	do {                                                                      \
 		const unsigned char *const _start = _hs - 1;                      \
@@ -148,13 +148,13 @@ pjstr_strrstr_mem_bmh(const unsigned char *JSTR_RST _hs,
 		   memset(_shift + 64 + 64, 0, 64),                               \
 		   memset(_shift + 64 + 64 + 64, 0, 64));                         \
 		for (ne_iterator_type _i = 1; _i < (ne_iterator_type)_mtc1; ++_i) \
-			_shift[PJSTR_HASH2(_ne + _i)] = _i;                       \
-		const size_t shft1 = _mtc1 - _shift[PJSTR_HASH2(_ne + _mtc1)];    \
-		_shift[PJSTR_HASH2(_ne + _mtc1)] = _mtc1;                         \
+			_shift[H(_ne + _i)] = _i;                                 \
+		const size_t shft1 = _mtc1 - _shift[H(_ne + _mtc1)];              \
+		_shift[H(_ne + _mtc1)] = _mtc1;                                   \
 		do {                                                              \
 			do {                                                      \
 				_hs -= _mtc1;                                     \
-				_tmp = _shift[PJSTR_HASH2(_hs)];                  \
+				_tmp = _shift[H(_hs)];                            \
 			} while (!_tmp - (_hs <= _start) == 0);                   \
 			_hs -= _tmp;                                              \
 			if (_mtc1 < 15 || !memcmp(_hs + _off, _ne + _off, 8)) {   \
@@ -169,7 +169,7 @@ pjstr_strrstr_mem_bmh(const unsigned char *JSTR_RST _hs,
 	if (jstr_unlikely(_nelen > 256))
 		PJSTR_STRRSTR_BMH(size_t, size_t);
 	PJSTR_STRRSTR_BMH(uint8_t, int);
-#undef PJSTR_HASH2
+#undef H
 #undef PJSTR_STRRSTR_BMH
 }
 
@@ -311,7 +311,7 @@ pjstr_strcasestr_mem_bmh(const char *JSTR_RST const _hs,
 			 const char *JSTR_RST const _ne,
 			 const size_t _nelen) JSTR_NOEXCEPT
 {
-#define PJSTR_HASH2_LOWER(p) (((size_t)(jstr_tolower_ascii((p)[0])) - ((size_t)jstr_tolower_ascii((p)[-1]) << 3)) % 256)
+#define HL(p) (((size_t)(jstr_tolower_ascii((p)[0])) - ((size_t)jstr_tolower_ascii((p)[-1]) << 3)) % 256)
 #define PJSTR_STRCASESTR_BMH(table_type, ne_iterator_type)                                                 \
 	do {                                                                                               \
 		const unsigned char *_h = (unsigned char *)_hs;                                            \
@@ -328,13 +328,13 @@ pjstr_strcasestr_mem_bmh(const char *JSTR_RST const _hs,
 		   memset(_shift + 64 + 64, 0, 64),                                                        \
 		   memset(_shift + 64 + 64 + 64, 0, 64));                                                  \
 		for (ne_iterator_type _i = 1; _i < (ne_iterator_type)_mtc1; ++_i)                          \
-			_shift[PJSTR_HASH2_LOWER(_n + _i)] = _i;                                           \
-		const size_t _shft1 = _mtc1 - _shift[PJSTR_HASH2_LOWER(_n + _mtc1)];                       \
-		_shift[PJSTR_HASH2_LOWER(_n + _mtc1)] = _mtc1;                                             \
+			_shift[HL(_n + _i)] = _i;                                                          \
+		const size_t _shft1 = _mtc1 - _shift[HL(_n + _mtc1)];                                      \
+		_shift[HL(_n + _mtc1)] = _mtc1;                                                            \
 		do {                                                                                       \
 			do {                                                                               \
 				_h += _mtc1;                                                               \
-				_tmp = _shift[PJSTR_HASH2_LOWER(_h)];                                      \
+				_tmp = _shift[HL(_h)];                                                     \
 			} while (!_tmp && (_h < _end));                                                    \
 			_h -= _tmp;                                                                        \
 			if (_tmp < _mtc1)                                                                  \
@@ -353,7 +353,7 @@ pjstr_strcasestr_mem_bmh(const char *JSTR_RST const _hs,
 	if (jstr_unlikely(_nelen > 256))
 		PJSTR_STRCASESTR_BMH(size_t, size_t);
 	PJSTR_STRCASESTR_BMH(uint8_t, int);
-#undef PJSTR_HASH2_LOWER
+#undef HL
 #undef PJSTR_STRCASESTR_BMH
 }
 
@@ -366,10 +366,6 @@ pjstr_strcasestr_bmh(const char *JSTR_RST const _hs,
 #define PJSTR_STRCASESTR_BMH(table_type, ne_iterator_type)                                                 \
 	do {                                                                                               \
 		const unsigned char *_h = (unsigned char *)_hs;                                            \
-		const char _ht[] = { (char)jstr_tolower_ascii(*_h), (char)jstr_toupper_ascii(*_h), '\0' }; \
-		_h = (unsigned char *)strpbrk((char *)_h, _ht);                                            \
-		if (_h == NULL)                                                                            \
-			return NULL;                                                                       \
 		size_t _hslen = jstr_strnlen((char *)_h, _nelen | 512);                                    \
 		if (_hslen < _nelen)                                                                       \
 			return NULL;                                                                       \
@@ -567,7 +563,7 @@ JSTR_FUNC_PURE
 JSTR_INLINE
 #endif
 static char *
-jstr_strcasestr(const char *JSTR_RST const _hs,
+jstr_strcasestr(const char *JSTR_RST _hs,
 		const char *JSTR_RST const _ne) JSTR_NOEXCEPT
 {
 #if JSTR_HAVE_STRCASESTR
@@ -575,23 +571,32 @@ jstr_strcasestr(const char *JSTR_RST const _hs,
 #else
 	if (jstr_unlikely(_ne[0] == '\0'))
 		return (char *)_hs;
+	const int _alpha1 = jstr_isalpha(*_ne);
+	if (_alpha1) {
+		const char _a[] = { (char)jstr_tolower_ascii(*_ne), (char)jstr_toupper_ascii(*_ne), '\0' };
+		_hs = strpbrk(_hs, _a);
+	} else {
+		_hs = strchr(_hs, (int)*_ne);
+	}
+	if (_hs == NULL)
+		return NULL;
 	if (_ne[1] == '\0')
-		return pstrcasechr(_hs, *_ne);
+		return (char *)_hs;
 	if (_ne[2] == '\0') {
-		if (jstr_isalpha(_ne[0])
+		if (_alpha1
 		    + jstr_isalpha(_ne[1])
 		    == 0)
 			return (char *)strstr(_hs, _ne);
 		return pjstr_strcasestr2((unsigned char *)_hs, (unsigned char *)_ne);
 	} else if (_ne[3] == '\0') {
-		if (jstr_isalpha(_ne[0])
+		if (_alpha1
 		    + jstr_isalpha(_ne[1])
 		    + jstr_isalpha(_ne[2])
 		    == 0)
 			return (char *)strstr(_hs, _ne);
 		return pjstr_strcasestr3((unsigned char *)_hs, (unsigned char *)_ne);
 	} else if (_ne[4] == '\0') {
-		if (jstr_isalpha(_ne[0])
+		if (_alpha1
 		    + jstr_isalpha(_ne[1])
 		    + jstr_isalpha(_ne[2])
 		    + jstr_isalpha(_ne[3])
