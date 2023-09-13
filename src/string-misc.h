34d3bf1675937383d57774e5cdf0ebd2f6b45534
James Tirta Halim <tirtajames45@gmail.com>
2023-09-14 00:25:13 +0700

    newest

--- src/string-misc.h
+++ src/string-misc.h
@@ -11,24 +11,43 @@ PJSTR_BEGIN_DECLS
 JSTR_INLINE
 JSTR_FUNC_PURE
 static pjstr_op_ty
-pjstr_ctoword(const char *JSTR_RESTRICT const ch)
+pjstr_ctoword_rev(const char *JSTR_RESTRICT const p)
+{
+	if (__BYTE_ORDER == __LITTLE_ENDIAN)
+		return (pjstr_op_ty)p[0] << 56 | (pjstr_op_ty)p[1] << 48 | (pjstr_op_ty)p[2] << 40 | (pjstr_op_ty)p[3] << 32 | (pjstr_op_ty)p[4] << 24 | (pjstr_op_ty)p[5] << 16 | (pjstr_op_ty)p[6] << 8 | (pjstr_op_ty)p[7];
+	else
+		return (pjstr_op_ty)p[0] >> 56 | (pjstr_op_ty)p[1] >> 48 | (pjstr_op_ty)p[2] >> 40 | (pjstr_op_ty)p[3] >> 32 | (pjstr_op_ty)p[4] >> 24 | (pjstr_op_ty)p[5] >> 16 | (pjstr_op_ty)p[6] >> 8 | (pjstr_op_ty)p[7];
+}
+
+JSTR_INLINE
+JSTR_FUNC_PURE
+static pjstr_op_ty
+pjstr_uctoword_rev(const unsigned char *JSTR_RESTRICT const p)
+{
+	return pjstr_ctoword_rev((char *)p);
+}
+
+JSTR_INLINE
+JSTR_FUNC_PURE
+static pjstr_op_ty
+pjstr_ctoword(const char *JSTR_RESTRICT const p)
 {
 #if JSTR_HAVE_ATTR_MAY_ALIAS
-	return *(pjstr_op_ty *)ch;
+	return *(pjstr_op_ty *)p;
 #else
 	if (__BYTE_ORDER == __LITTLE_ENDIAN)
-		return (pjstr_op_ty)ch[7] << 56 | (pjstr_op_ty)ch[6] << 48 | (pjstr_op_ty)ch[5] << 40 | (pjstr_op_ty)ch[4] << 32 | (pjstr_op_ty)ch[3] << 24 | (pjstr_op_ty)ch[2] << 16 | (pjstr_op_ty)ch[1] << 8 | (pjstr_op_ty)ch[0];
+		return (pjstr_op_ty)p[7] << 56 | (pjstr_op_ty)p[6] << 48 | (pjstr_op_ty)p[5] << 40 | (pjstr_op_ty)p[4] << 32 | (pjstr_op_ty)p[3] << 24 | (pjstr_op_ty)p[2] << 16 | (pjstr_op_ty)p[1] << 8 | (pjstr_op_ty)p[0];
 	else
-		return (pjstr_op_ty)ch[7] >> 56 | (pjstr_op_ty)ch[6] >> 48 | (pjstr_op_ty)ch[5] >> 40 | (pjstr_op_ty)ch[4] >> 32 | (pjstr_op_ty)ch[3] >> 24 | (pjstr_op_ty)ch[2] >> 16 | (pjstr_op_ty)ch[1] >> 8 | (pjstr_op_ty)ch[0];
+		return (pjstr_op_ty)p[7] >> 56 | (pjstr_op_ty)p[6] >> 48 | (pjstr_op_ty)p[5] >> 40 | (pjstr_op_ty)p[4] >> 32 | (pjstr_op_ty)p[3] >> 24 | (pjstr_op_ty)p[2] >> 16 | (pjstr_op_ty)p[1] >> 8 | (pjstr_op_ty)p[0];
 #endif
 }
 
 JSTR_INLINE
 JSTR_FUNC_PURE
 static pjstr_op_ty
-pjstr_uctoword(const unsigned char *JSTR_RESTRICT const ch)
+pjstr_uctoword(const unsigned char *JSTR_RESTRICT const p)
 {
-	return pjstr_ctoword((char *)ch);
+	return pjstr_ctoword((char *)p);
 }
 
 PJSTR_END_DECLS
