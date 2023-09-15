f1a85d6a7fbb04c6f8bb8a9982e7aeb1b2595acf
James Tirta Halim <tirtajames45@gmail.com>
2023-09-15 16:36:46 +0700

    newest

--- src/jstr-macros.h
+++ src/jstr-macros.h
@@ -712,16 +712,18 @@ case '~':
 #	define JSTR_ARCH_GENERIC 1
 #endif
 
-#if defined __BYTE_ORDER && __BYTE_ORDER == __BIG_ENDIAN || defined __BIG_ENDIAN__ || defined __ARMEB__ || defined __THUMBEB__ || defined __AARCH64EB__ || defined _MIBSEB || defined __MIBSEB || defined __MIBSEB__
-#	undef JSTR_ENDIAN_BIG
-#	undef JSTR_ENDIAN_LITTLE
-#	define JSTR_ENDIAN_BIG	   1
-#	define JSTR_ENDIAN_LITTLE 0
-#elif defined __BYTE_ORDER && __BYTE_ORDER == __LITTLE_ENDIAN || defined __LITTLE_ENDIAN__ || defined __ARMEL__ || defined __THUMBEL__ || defined __AARCH64EL__ || defined _MIPSEL || defined __MIPSEL || defined __MIPSEL__
-#	undef JSTR_ENDIAN_LITTLE
-#	undef JSTR_ENDIAN_BIG
-#	define JSTR_ENDIAN_LITTLE 1
-#	define JSTR_ENDIAN_BIG	   0
+#ifdef __BYTE_ORDER
+#	if __BYTE_ORDER == __BIG_ENDIAN || defined __BIG_ENDIAN__ || defined __ARMEB__ || defined __THUMBEB__ || defined __AARCH64EB__ || defined _MIBSEB || defined __MIBSEB || defined __MIBSEB__
+#		undef JSTR_ENDIAN_BIG
+#		undef JSTR_ENDIAN_LITTLE
+#		define JSTR_ENDIAN_BIG	   1
+#		define JSTR_ENDIAN_LITTLE 0
+#	elif __BYTE_ORDER == __LITTLE_ENDIAN || defined __LITTLE_ENDIAN__ || defined __ARMEL__ || defined __THUMBEL__ || defined __AARCH64EL__ || defined _MIPSEL || defined __MIPSEL || defined __MIPSEL__
+#		undef JSTR_ENDIAN_LITTLE
+#		undef JSTR_ENDIAN_BIG
+#		define JSTR_ENDIAN_LITTLE 1
+#		define JSTR_ENDIAN_BIG	   0
+#	endif
 #endif
 
 #define JSTR_FUNC_VOID	      JSTR_NOTHROW JSTR_MAYBE_UNUSED JSTR_NONNULL_ALL
