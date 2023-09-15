53bd91d40f0517b6db345f366cd75be52e6acc85
James Tirta Halim <tirtajames45@gmail.com>
2023-09-15 11:05:03 +0700

    newest

--- src/string-misc.h
+++ src/string-misc.h
@@ -11,25 +11,6 @@ PJSTR_BEGIN_DECLS
 JSTR_INLINE
 JSTR_FUNC_PURE
 static jstr_word_ty
-jstr_word_ctow_rev(const char *JSTR_RESTRICT const p)
-{
-	if (__BYTE_ORDER == __LITTLE_ENDIAN)
-		return (jstr_word_ty)p[0] << 56 | (jstr_word_ty)p[1] << 48 | (jstr_word_ty)p[2] << 40 | (jstr_word_ty)p[3] << 32 | (jstr_word_ty)p[4] << 24 | (jstr_word_ty)p[5] << 16 | (jstr_word_ty)p[6] << 8 | (jstr_word_ty)p[7];
-	else
-		return (jstr_word_ty)p[0] >> 56 | (jstr_word_ty)p[1] >> 48 | (jstr_word_ty)p[2] >> 40 | (jstr_word_ty)p[3] >> 32 | (jstr_word_ty)p[4] >> 24 | (jstr_word_ty)p[5] >> 16 | (jstr_word_ty)p[6] >> 8 | (jstr_word_ty)p[7];
-}
-
-JSTR_INLINE
-JSTR_FUNC_PURE
-static jstr_word_ty
-jstr_word_uctow_rev(const unsigned char *JSTR_RESTRICT const p)
-{
-	return jstr_word_ctow_rev((char *)p);
-}
-
-JSTR_INLINE
-JSTR_FUNC_PURE
-static jstr_word_ty
 jstr_word_ctow(const char *JSTR_RESTRICT const p)
 {
 #if JSTR_HAVE_ATTR_MAY_ALIAS
@@ -48,6 +29,25 @@ static jstr_word_ty
 jstr_word_uctow(const unsigned char *JSTR_RESTRICT const p)
 {
 	return jstr_word_ctow((char *)p);
+}
+
+JSTR_INLINE
+JSTR_FUNC_PURE
+static jstr_word_ty
+jstr_word_ctow_rev(const char *JSTR_RESTRICT const p)
+{
+	if (__BYTE_ORDER == __LITTLE_ENDIAN)
+		return (jstr_word_ty)p[0] << 56 | (jstr_word_ty)p[1] << 48 | (jstr_word_ty)p[2] << 40 | (jstr_word_ty)p[3] << 32 | (jstr_word_ty)p[4] << 24 | (jstr_word_ty)p[5] << 16 | (jstr_word_ty)p[6] << 8 | (jstr_word_ty)p[7];
+	else
+		return (jstr_word_ty)p[0] >> 56 | (jstr_word_ty)p[1] >> 48 | (jstr_word_ty)p[2] >> 40 | (jstr_word_ty)p[3] >> 32 | (jstr_word_ty)p[4] >> 24 | (jstr_word_ty)p[5] >> 16 | (jstr_word_ty)p[6] >> 8 | (jstr_word_ty)p[7];
+}
+
+JSTR_INLINE
+JSTR_FUNC_PURE
+static jstr_word_ty
+jstr_word_uctow_rev(const unsigned char *JSTR_RESTRICT const p)
+{
+	return jstr_word_ctow_rev((char *)p);
 }
 
 PJSTR_END_DECLS
