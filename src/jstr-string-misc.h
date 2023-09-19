98266134812df640dabc1abceca1c0d92fb17da7
James Tirta Halim <tirtajames45@gmail.com>
2023-09-19 21:43:07 +0700

    newest

--- src/jstr-string-misc.h
+++ src/jstr-string-misc.h
@@ -83,7 +83,6 @@ jstr_word_ctow(const char *JSTR_RESTRICT const p)
 	case 63: return SH(62) | SH(61) | SH(60) | SH(59) | SH(58) | SH(57) | SH(56) | SH(55) | SH(54) | SH(53) | SH(52) | SH(51) | SH(50) | SH(49) | SH(48) | SH(47) | SH(46) | SH(45) | SH(44) | SH(43) | SH(42) | SH(41) | SH(40) | SH(39) | SH(38) | SH(37) | SH(36) | SH(35) | SH(34) | SH(33) | SH(32) | SH(31) | SH(30) | SH(29) | SH(28) | SH(27) | SH(26) | SH(25) | SH(24) | SH(23) | SH(22) | SH(21) | SH(20) | SH(19) | SH(18) | SH(17) | SH(16) | SH(15) | SH(14) | SH(13) | SH(12) | SH(11) | SH(10) | SH(9) | SH(8) | SH(7) | SH(6) | SH(5) | SH(4) | SH(3) | SH(2) | SH(1) | SH(0);
 	case 64: return SH(63) | SH(62) | SH(61) | SH(60) | SH(59) | SH(58) | SH(57) | SH(56) | SH(55) | SH(54) | SH(53) | SH(52) | SH(51) | SH(50) | SH(49) | SH(48) | SH(47) | SH(46) | SH(45) | SH(44) | SH(43) | SH(42) | SH(41) | SH(40) | SH(39) | SH(38) | SH(37) | SH(36) | SH(35) | SH(34) | SH(33) | SH(32) | SH(31) | SH(30) | SH(29) | SH(28) | SH(27) | SH(26) | SH(25) | SH(24) | SH(23) | SH(22) | SH(21) | SH(20) | SH(19) | SH(18) | SH(17) | SH(16) | SH(15) | SH(14) | SH(13) | SH(12) | SH(11) | SH(10) | SH(9) | SH(8) | SH(7) | SH(6) | SH(5) | SH(4) | SH(3) | SH(2) | SH(1) | SH(0);
 	}
-#	undef P_JSTR_WORD_CTOW
 #	undef SH
 #endif
 }
