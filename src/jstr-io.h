86803161225da5f5c0221bba74c96b349cbf5dcc
James Tirta Halim <tirtajames45@gmail.com>
2023-10-03 21:09:23 +0700

    new

--- src/jstr-io.h
+++ src/jstr-io.h
@@ -941,7 +941,7 @@ pjstr_io_ftw_len(char *R dirpath,
 			goto err_closedir;
 		}
 #else
-		/* Approximate dlen before calling strlen(). */
+		/* Approximate length before calling strlen(). */
 		if (jstr_unlikely(dlen >= JSTR_IO_PATH_MAX - JSTR_IO_NAME_MAX)
 		    && dlen + strlen(ep->d_name) >= JSTR_IO_PATH_MAX) {
 			errno = ENAMETOOLONG;
