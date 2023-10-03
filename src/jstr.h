843553dff13ebcb5076a6263f89e37738fd959c0
James Tirta Halim <tirtajames45@gmail.com>
2023-10-03 19:07:22 +0700

    new

--- src/jstr.h
+++ src/jstr.h
@@ -3,9 +3,7 @@
 
 #include "jstr-builder.h"
 #include "jstr-ctype.h"
-#include "jstr-io.h"
 #include "jstr-macros.h"
-#include "jstr-regex.h"
 #include "jstr-replace.h"
 #include "jstr-string.h"
 #include "jstr-itoa.h"
