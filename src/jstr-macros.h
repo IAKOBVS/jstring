6c90627e60319fdc08faa7662b9f437791e77e33
James Tirta Halim <tirtajames45@gmail.com>
2023-10-16 21:58:08 +0700

    new

--- src/jstr-macros.h
+++ src/jstr-macros.h
@@ -995,10 +995,12 @@ case '~':
 #if defined __GLIBC__ && (JSTR_ARCH_X86_64 || JSTR_ARCH_POWERPC7 || JSTR_ARCH_POWERPC64 || JSTR_ARCH_S390)
 #	define JSTR_HAVE_STRSTR_OPTIMIZED 1
 #endif
-/* Needle length over which memmem would be faster than strstr. */
-enum { JSTR_MEMMEM_THRES = 18,
-       /* Number of iterations over which an optimized strcspn would be faster than a byte loop. */
-       JSTR_STRCASECHR_STRCSPN_THRES = 24 };
+enum {
+	/* Needle length over which memmem would be faster than strstr. */
+	JSTR_MEMMEM_THRES = 18,
+	/* Iterations over which an optimized strcspn would be faster than a byte loop. */
+	JSTR_STRCASECHR_STRCSPN_THRES = 24
+};
 
 /* Only use memmem for long needles or when it is implemented in assembly.
    It seems to be slower than strstr for short needles. */
