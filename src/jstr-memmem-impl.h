5e1d2240e4ad8b8e092326d5494d4f67269b1688
James Tirta Halim <tirtajames45@gmail.com>
2023-10-19 22:49:41 +0700

    new

--- src/jstr-memmem-impl.h
+++ src/jstr-memmem-impl.h
@@ -60,7 +60,7 @@ JSTR_NOEXCEPT
 	shift[PJSTR_MEMMEM_HASH2(ne + m1)] = m1;
 	goto start;
 	do {
-#if PJSTR_MEMMEM_CHECK_EOL && !PJSTR_MEMMEM_REVESE /* As in strstr() where we don't know the haystacklen and need to update it. */
+#if PJSTR_MEMMEM_CHECK_EOL && !PJSTR_MEMMEM_REVESE /* As in strstr() where we don't know where HAYSTACKLEN needs to be updated. */
 		if (jstr_unlikely(hs > end)) {
 			end += jstr_strnlen((char *)(end + m1), 2048);
 			if (hs > end)
