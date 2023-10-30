e890d105743ccf14e1319d9a7998889918e54060
James Tirta Halim <tirtajames45@gmail.com>
2023-10-30 14:25:02 +0700

    new

---
+++ src/update-glibc.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+while (glob('./glibc/sysdeps/*/string-*.h')) {
+	/([^\/]*\/[^\/]*)$/;
+	my $new_fname = $1;
+	$new_fname =~ s/\//-/;
+	$new_fname = 'glibc-' . $new_fname;
+	system("cp -f $_ $new_fname");
+}
