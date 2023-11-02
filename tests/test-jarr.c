041f229536ef0035550810b4ba09f2f2fe451b37
James Tirta Halim <tirtajames45@gmail.com>
2023-11-02 20:29:13 +0700

    new

--- tests/test-jarr.c
+++ tests/test-jarr.c
@@ -6,7 +6,6 @@
 		int expected[] = { __VA_ARGS__ };                 \
 		func;                                             \
 		assert(j.data);                                   \
-		assert(j.size == ASZ(expected));                  \
 		assert(!memcmp(j.data, expected, ASZ(expected))); \
 	} while (0)
 
@@ -14,10 +13,14 @@ int
 main()
 {
 	jarr_ty(int, j) = JARR_INIT;
-	T(jarr_cat(&j, 1, 2, 3, 4, 5), 1, 2, 3, 4, 5);
-	T(jarr_push_back(&j, 6), 1, 2, 3, 4, 5, 6);
-	T(jarr_pop_back(&j), 1, 2, 3, 4, 5);
-	T(jarr_pop_front(&j), 2, 3, 4, 5);
+	T(jarr_cat(&j, 1, 2, 3, 4, 5),
+			1, 2, 3, 4, 5);
+	T(jarr_push_back(&j, 6),
+			1, 2, 3, 4, 5, 6);
+	T(jarr_pop_back(&j),
+			1, 2, 3, 4, 5);
+	T(jarr_pop_front(&j),
+			2, 3, 4, 5);
 	jarr_free(&j);
 	return 0;
 }
