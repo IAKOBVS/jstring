50967124fddc4b806ba86aa53e4d8bb3630f0f0e
James Tirta Halim <tirtajames45@gmail.com>
2023-11-03 15:56:16 +0700

    new

---
+++ src/jstr-struct.h
@@ -0,0 +1,20 @@
+#ifndef JSTR_STRUCT_H
+#define JSTR_STRUCT_H 1
+
+#include "_jstr-macros.h"
+
+PJSTR_BEGIN_DECLS
+#include <stddef.h>
+PJSTR_END_DECLS
+
+PJSTR_BEGIN_DECLS
+
+typedef struct jstr_ty {
+	char *data;
+	size_t size;
+	size_t capacity;
+} jstr_ty;
+
+PJSTR_END_DECLS
+
+#endif /* JSTR_STRUCT_H */
