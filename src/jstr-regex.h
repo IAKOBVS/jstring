a1812acc343927ec3ec9092e250542bd165a7c40
James Tirta Halim <tirtajames45@gmail.com>
2023-11-04 17:39:17 +0700

    new

--- src/jstr-regex.h
+++ src/jstr-regex.h
@@ -762,11 +762,18 @@ JSTR_NOEXCEPT
 		pjstr_re_creatrplcbref((u *)p, rm, (u *)rdstp, (u *)rplc, rplc_len);
 		p += rm[0].rm_so;
 		if (rdst_len <= find_len)
+#ifdef __GNUC__
+#	pragma GCC diagnostic ignored "-Wanalyzer-use-of-uninitialized-value"
+#	pragma GCC diagnostic push
+#endif
 			pjstr_rplcallinplace(&dst, &oldp, (const u **)&p, rdstp, rdst_len, find_len);
 		else if (*cap > *sz + rdst_len - find_len)
 			pjstr_re_rplcall_small_rplc(*(u **)s, sz, &dst, &oldp, &p, rdstp, rdst_len, find_len);
 		else if (jstr_unlikely(!pjstr_re_rplcall_big_rplc((u **)s, sz, cap, &dst, &oldp, &p, rdstp, rdst_len, find_len)))
 			goto err_free;
+#ifdef __GNUC__
+#	pragma GCC diagnostic pop
+#endif
 	}
 	if (dst != oldp)
 		*sz = jstr_stpmove_len(dst, oldp, (*(u **)s + *sz) - oldp) - *s;
