9906bac313c02f17553aaad01f51ec27ad41919c
James Tirta Halim <tirtajames45@gmail.com>
2023-11-10 10:59:48 +0700

    new

---
+++ src/jstr-word-at-a-time.h
@@ -0,0 +1,17 @@
+#ifndef JSTR_WORD_AT_A_TIME_H
+#define JSTR_WORD_AT_A_TIME_H
+
+#include "jstr-macros.h"
+
+#if JSTR_HAVE_WORD_AT_A_TIME
+#	include "_string-fza.h"
+#	include "_string-fzb.h"
+#	include "_string-fzc.h"
+#	include "_string-fzi.h"
+#	include "_string-misc.h"
+#	include "_string-opthr.h"
+#	include "_string-optype.h"
+#	include "_string-shift.h"
+#endif
+
+#endif /* JSTR_WORD_AT_A_TIME_H */
