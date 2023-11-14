86450e3673ad72c684a508b5f8d86a1e9b27f086
James Tirta Halim <tirtajames45@gmail.com>
2023-11-14 17:11:52 +0700

    new

--- src/jarr.h
+++ src/jarr.h
@@ -17,9 +17,9 @@ JSTRP_END_DECLS
 
 #define R JSTR_RESTRICT
 
-#define PJARR_DATA_NAME     data
-#define PJARR_SIZE_NAME     size
-#define PJARR_CAPACITY_NAME capacity
+#define JARRP_DATA_NAME     data
+#define JARRP_SIZE_NAME     size
+#define JARRP_CAPACITY_NAME capacity
 
 #define JARR_INIT \
 	{         \
@@ -28,191 +28,191 @@ JSTRP_END_DECLS
 
 #define jarr_ty(T, name)                    \
 	typedef struct pjarr_##name##_ty {  \
-		T *PJARR_DATA_NAME;         \
-		size_t PJARR_SIZE_NAME;     \
-		size_t PJARR_CAPACITY_NAME; \
+		T *JARRP_DATA_NAME;         \
+		size_t JARRP_SIZE_NAME;     \
+		size_t JARRP_CAPACITY_NAME; \
 	} jarr_##name##_ty;                 \
 	jarr_##name##_ty name
 
-#define PJARR_ELEMSZ(j) (sizeof((PJARR_DATA(j))[0]))
-#define PJARR_ARRSZ(j)  (sizeof(PJARR_DATA(j)) / sizeof((PJARR_DATA(j))[0]))
-#define PJARR_DATA(j)   ((j)->PJARR_DATA_NAME)
-#define PJARR_SZ(j)     ((j)->PJARR_SIZE_NAME)
-#define PJARR_CAP(j)    ((j)->PJARR_CAPACITY_NAME)
+#define JARRP_ELEMSZ(j) (sizeof((JARRP_DATA(j))[0]))
+#define JARRP_ARRSZ(j)  (sizeof(JARRP_DATA(j)) / sizeof((JARRP_DATA(j))[0]))
+#define JARRP_DATA(j)   ((j)->JARRP_DATA_NAME)
+#define JARRP_SZ(j)     ((j)->JARRP_SIZE_NAME)
+#define JARRP_CAP(j)    ((j)->JARRP_CAPACITY_NAME)
 
-#define PJARR_MIN_CAP(j) (JSTR_MIN_CAP / PJARR_ELEMSZ(j))
+#define JARRP_MIN_CAP(j) (JSTR_MIN_CAP / JARRP_ELEMSZ(j))
 
-#define PJARR_MEMMOVE(j, dst, src, n) memmove(dst, src, (n)*PJARR_ELEMSZ(j))
-#define PJARR_MEMCPY(j, dst, src, n)  memcpy(dst, src, (n)*PJARR_ELEMSZ(j))
+#define JARRP_MEMMOVE(j, dst, src, n) memmove(dst, src, (n)*JARRP_ELEMSZ(j))
+#define JARRP_MEMCPY(j, dst, src, n)  memcpy(dst, src, (n)*JARRP_ELEMSZ(j))
 
-#define jarr_chk(j)        (jstr_unlikely(PJARR_DATA(j) == NULL))
+#define jarr_chk(j)        (jstr_unlikely(JARRP_DATA(j) == NULL))
 #define jarr_err(msg)      jstr_err(msg)
 #define jarr_err_exit(msg) jstr_err_exit(msg)
 
-#define PJARR_ALIGN_UP(j, base) ((PJARR_ELEMSZ(j) < (sizeof(size_t) + sizeof(size_t))) ? JSTR_ALIGN_UP_STR(base) : base)
+#define JARRP_ALIGN_UP(j, base) ((JARRP_ELEMSZ(j) < (sizeof(size_t) + sizeof(size_t))) ? JSTR_ALIGN_UP_STR(base) : base)
 
 #if JSTR_DEBUG || JSTR_EXIT_ON_ERROR
-#	define PJARR_MALLOC_ERR(j, do_on_malloc_err)         \
-		if (jstr_unlikely((PJARR_DATA(j)) == NULL)) { \
-			PJARR_NULLIFY_MEMBERS(j);             \
+#	define JARRP_MALLOC_ERR(j, do_on_malloc_err)         \
+		if (jstr_unlikely((JARRP_DATA(j)) == NULL)) { \
+			JARRP_NULLIFY_MEMBERS(j);             \
 			do_on_malloc_err;                     \
 			jstr_err_exit("");                    \
 		}
 #else
-#	define PJARR_MALLOC_ERR(j, do_on_malloc_err)         \
-		if (jstr_unlikely((PJARR_DATA(j)) == NULL)) { \
-			PJARR_NULLIFY_MEMBERS(j);             \
+#	define JARRP_MALLOC_ERR(j, do_on_malloc_err)         \
+		if (jstr_unlikely((JARRP_DATA(j)) == NULL)) { \
+			JARRP_NULLIFY_MEMBERS(j);             \
 			do_on_malloc_err;                     \
 		}
 #endif
 
-#define PJARR_CHECK_ARG(j)                         \
+#define JARRP_CHECK_ARG(j)                         \
 	do {                                       \
-		JSTR_ASSERT_IS_SIZE(PJARR_SZ(j));  \
-		JSTR_ASSERT_IS_SIZE(PJARR_CAP(j)); \
+		JSTR_ASSERT_IS_SIZE(JARRP_SZ(j));  \
+		JSTR_ASSERT_IS_SIZE(JARRP_CAP(j)); \
 	} while (0)
-#define PJARR_GROW(old_cap, new_cap)                                        \
+#define JARRP_GROW(old_cap, new_cap)                                        \
 	do {                                                                \
 		JSTR_ASSERT_IS_SIZE(old_cap);                               \
 		JSTR_ASSERT_IS_SIZE(new_cap);                               \
 		if (jstr_unlikely(old_cap == 0))                            \
-			old_cap = JSTR_MIN_CAP / PJARR_ALLOC_MULTIPLIER;    \
+			old_cap = JSTR_MIN_CAP / JARRP_ALLOC_MULTIPLIER;    \
 		do                                                          \
-			(old_cap) *= PJARR_GROWTH;                          \
+			(old_cap) *= JARRP_GROWTH;                          \
 		while ((old_cap) < (new_cap));                              \
 		(old_cap) = JSTR_ALIGN_UP(old_cap, JSTRP_MALLOC_ALIGNMENT); \
 	} while (0)
 #define jarr_reserve(j, new_cap)                                                                                   \
 	do {                                                                                                       \
-		PJARR_CHECK_ARG(j);                                                                                \
-		if (jstr_unlikely(PJARR_CAP(j) == 0))                                                              \
-			PJARR_CAP(j) = JSTR_MIN_CAP / PJARR_ELEMSZ(j);                                             \
-		PJARR_GROW(PJARR_CAP(j), new_cap);                                                                 \
-		PJARR_CAP(j) = PJARR_ALIGN_UP(j, PJARR_CAP(j) * PJARR_ELEMSZ(j));                                  \
-		PJARR_DATA(j) = JSTRP_CAST(PJARR_DATA(j), realloc(PJARR_DATA(j), PJARR_CAP(j) * PJARR_ELEMSZ(j))); \
+		JARRP_CHECK_ARG(j);                                                                                \
+		if (jstr_unlikely(JARRP_CAP(j) == 0))                                                              \
+			JARRP_CAP(j) = JSTR_MIN_CAP / JARRP_ELEMSZ(j);                                             \
+		JARRP_GROW(JARRP_CAP(j), new_cap);                                                                 \
+		JARRP_CAP(j) = JARRP_ALIGN_UP(j, JARRP_CAP(j) * JARRP_ELEMSZ(j));                                  \
+		JARRP_DATA(j) = JSTRP_CAST(JARRP_DATA(j), realloc(JARRP_DATA(j), JARRP_CAP(j) * JARRP_ELEMSZ(j))); \
 		if (jarr_chk(j))                                                                                   \
 			break;                                                                                     \
-		PJARR_CAP(j) /= PJARR_ELEMSZ(j);                                                                   \
+		JARRP_CAP(j) /= JARRP_ELEMSZ(j);                                                                   \
 	} while (0)
 #define jarr_reserveexact(j, new_cap)                                                                              \
 	do {                                                                                                       \
-		PJARR_CHECK_ARG(j);                                                                                \
-		PJARR_CAP(j) = PJARR_ALIGN_UP(j, PJARR_CAP(j) * PJARR_ELEMSZ(j));                                  \
-		PJARR_DATA(j) = JSTRP_CAST(PJARR_DATA(j), realloc(PJARR_DATA(j), PJARR_CAP(j) * PJARR_ELEMSZ(j))); \
+		JARRP_CHECK_ARG(j);                                                                                \
+		JARRP_CAP(j) = JARRP_ALIGN_UP(j, JARRP_CAP(j) * JARRP_ELEMSZ(j));                                  \
+		JARRP_DATA(j) = JSTRP_CAST(JARRP_DATA(j), realloc(JARRP_DATA(j), JARRP_CAP(j) * JARRP_ELEMSZ(j))); \
 		if (jarr_chk(j))                                                                                   \
 			break;                                                                                     \
-		PJARR_CAP(j) /= PJARR_ELEMSZ(j);                                                                   \
+		JARRP_CAP(j) /= JARRP_ELEMSZ(j);                                                                   \
 	} while (0)
 #if JSTR_HAVE_GENERIC && JSTR_HAVE_TYPEOF
-#	define PJARR_CHECK_VAL(j, value) JSTR_ASSERT(JSTR_SAME_TYPE(value, *PJARR_DATA(j)), "Passing illegal value incompatible with the array type.")
+#	define JARRP_CHECK_VAL(j, value) JSTR_ASSERT(JSTR_SAME_TYPE(value, *JARRP_DATA(j)), "Passing illegal value incompatible with the array type.")
 #else
-#	define PJARR_CHECK_VAL(j, value) \
+#	define JARRP_CHECK_VAL(j, value) \
 		do {                      \
 		} while (0)
 #endif
 
-#define PJARR_NULLIFY_MEMBERS(j)  \
+#define JARRP_NULLIFY_MEMBERS(j)  \
 	do {                      \
-		PJARR_SZ(j) = 0;  \
-		PJARR_CAP(j) = 0; \
+		JARRP_SZ(j) = 0;  \
+		JARRP_CAP(j) = 0; \
 	} while (0)
-#define PJARR_NULLIFY(j)                  \
+#define JARRP_NULLIFY(j)                  \
 	do {                              \
-		PJARR_DATA(j) = NULL;     \
-		PJARR_NULLIFY_MEMBERS(j); \
+		JARRP_DATA(j) = NULL;     \
+		JARRP_NULLIFY_MEMBERS(j); \
 	} while (0)
 
 #define jarr_free(j)                 \
 	do {                         \
-		free(PJARR_DATA(j)); \
-		PJARR_NULLIFY(j);    \
+		free(JARRP_DATA(j)); \
+		JARRP_NULLIFY(j);    \
 	} while (0)
 
 /* Add elements to end of PTR. */
 #define jarr_assign(j, ...)                                                     \
 	do {                                                                    \
-		PJARR_CHECK_ARG(j);                                             \
-		PJARR_CHECK_VAL(j, JSTRP_PP_FIRST_ARG(__VA_ARGS__));            \
-		if (jstr_unlikely(PJARR_CAP(j) < JSTRP_PP_NARG(__VA_ARGS__))) { \
+		JARRP_CHECK_ARG(j);                                             \
+		JARRP_CHECK_VAL(j, JSTRP_PP_FIRST_ARG(__VA_ARGS__));            \
+		if (jstr_unlikely(JARRP_CAP(j) < JSTRP_PP_NARG(__VA_ARGS__))) { \
 			jarr_reserve(j, JSTRP_PP_NARG(__VA_ARGS__));            \
-			PJARR_MALLOC_ERR(j, break)                              \
+			JARRP_MALLOC_ERR(j, break)                              \
 		}                                                               \
-		JSTRP_PP_ARRCPY_VA_ARGS(PJARR_SZ(j), __VA_ARGS__);              \
-		PJARR_SZ(j) = JSTRP_PP_NARG(__VA_ARGS__);                       \
+		JSTRP_PP_ARRCPY_VA_ARGS(JARRP_SZ(j), __VA_ARGS__);              \
+		JARRP_SZ(j) = JSTRP_PP_NARG(__VA_ARGS__);                       \
 	} while (0)
 
 /* Add elements to end of PTR. */
 #define jarr_cat(j, ...)                                                                        \
 	do {                                                                                    \
-		PJARR_CHECK_ARG(j);                                                             \
-		PJARR_CHECK_VAL(j, JSTRP_PP_FIRST_ARG(__VA_ARGS__));                            \
-		if (jstr_unlikely(PJARR_CAP(j) < (PJARR_SZ(j) + JSTRP_PP_NARG(__VA_ARGS__)))) { \
-			jarr_reserve(j, (PJARR_SZ(j) + JSTRP_PP_NARG(__VA_ARGS__)));            \
-			PJARR_MALLOC_ERR(j, break)                                              \
+		JARRP_CHECK_ARG(j);                                                             \
+		JARRP_CHECK_VAL(j, JSTRP_PP_FIRST_ARG(__VA_ARGS__));                            \
+		if (jstr_unlikely(JARRP_CAP(j) < (JARRP_SZ(j) + JSTRP_PP_NARG(__VA_ARGS__)))) { \
+			jarr_reserve(j, (JARRP_SZ(j) + JSTRP_PP_NARG(__VA_ARGS__)));            \
+			JARRP_MALLOC_ERR(j, break)                                              \
 		}                                                                               \
-		JSTRP_PP_ARRCPY_VA_ARGS(PJARR_DATA(j) + PJARR_SZ(j), __VA_ARGS__);              \
-		PJARR_SZ(j) += JSTRP_PP_NARG(__VA_ARGS__);                                      \
+		JSTRP_PP_ARRCPY_VA_ARGS(JARRP_DATA(j) + JARRP_SZ(j), __VA_ARGS__);              \
+		JARRP_SZ(j) += JSTRP_PP_NARG(__VA_ARGS__);                                      \
 	} while (0)
 /* Pop PTR[0]. */
 #define jarr_popfront(j)                                                  \
 	do {                                                              \
-		PJARR_CHECK_ARG(j);                                       \
-		if (jstr_unlikely(PJARR_CAP(j) == 0)) {                   \
-			PJARR_NULLIFY_MEMBERS(j);                         \
+		JARRP_CHECK_ARG(j);                                       \
+		if (jstr_unlikely(JARRP_CAP(j) == 0)) {                   \
+			JARRP_NULLIFY_MEMBERS(j);                         \
 			break;                                            \
 		}                                                         \
-		memmove(PJARR_DATA(j), PJARR_DATA(j) + 1, --PJARR_SZ(j)); \
+		memmove(JARRP_DATA(j), JARRP_DATA(j) + 1, --JARRP_SZ(j)); \
 	} while (0)
 /* Pop end of PTR. */
 #define jarr_popback(j)                                  \
 	do {                                             \
-		PJARR_CHECK_ARG(j);                      \
-		if (jstr_unlikely(PJARR_CAP(j) == 0)) {  \
-			PJARR_NULLIFY_MEMBERS(j);        \
+		JARRP_CHECK_ARG(j);                      \
+		if (jstr_unlikely(JARRP_CAP(j) == 0)) {  \
+			JARRP_NULLIFY_MEMBERS(j);        \
 			break;                           \
 		}                                        \
-		*(PJARR_DATA(j) + --PJARR_SZ(j)) = '\0'; \
+		*(JARRP_DATA(j) + --JARRP_SZ(j)) = '\0'; \
 	} while (0)
 /* Push VAL to back of PTR. */
 #define jarr_pushback(j, value)                                            \
 	do {                                                               \
-		PJARR_CHECK_ARG(j);                                        \
-		PJARR_CHECK_VAL(j, value);                                 \
-		if (jstr_unlikely(PJARR_CAP(j) < PJARR_SZ(j) + 1)) {       \
-			if (jstr_unlikely(PJARR_CAP(j) == 0))              \
-				PJARR_CAP(j) = PJARR_MIN_CAP(j);           \
-			jarr_reserveexact(j, PJARR_CAP(j) * PJARR_GROWTH); \
-			PJARR_MALLOC_ERR(j, break)                         \
+		JARRP_CHECK_ARG(j);                                        \
+		JARRP_CHECK_VAL(j, value);                                 \
+		if (jstr_unlikely(JARRP_CAP(j) < JARRP_SZ(j) + 1)) {       \
+			if (jstr_unlikely(JARRP_CAP(j) == 0))              \
+				JARRP_CAP(j) = JARRP_MIN_CAP(j);           \
+			jarr_reserveexact(j, JARRP_CAP(j) * JARRP_GROWTH); \
+			JARRP_MALLOC_ERR(j, break)                         \
 		}                                                          \
-		*(PJARR_DATA(j) + PJARR_SZ(j)++) = (value);                \
+		*(JARRP_DATA(j) + JARRP_SZ(j)++) = (value);                \
 	} while (0)
 /* Push VAL to front of P. */
 #define jarr_pushfront(j, value)                                                   \
 	do {                                                                       \
-		PJARR_CHECK_ARG(j);                                                \
-		PJARR_CHECK_VAL(j, value);                                         \
-		if (jstr_unlikely(PJARR_CAP(j) < PJARR_SZ(j) + 1)) {               \
-			if (jstr_unlikely(PJARR_CAP(j) == 0))                      \
-				PJARR_CAP(j) = PJARR_MIN_CAP(j);                   \
-			jarr_reserveexact(j, PJARR_CAP(j) * PJARR_GROWTH);         \
-			PJARR_MALLOC_ERR(j, break)                                 \
+		JARRP_CHECK_ARG(j);                                                \
+		JARRP_CHECK_VAL(j, value);                                         \
+		if (jstr_unlikely(JARRP_CAP(j) < JARRP_SZ(j) + 1)) {               \
+			if (jstr_unlikely(JARRP_CAP(j) == 0))                      \
+				JARRP_CAP(j) = JARRP_MIN_CAP(j);                   \
+			jarr_reserveexact(j, JARRP_CAP(j) * JARRP_GROWTH);         \
+			JARRP_MALLOC_ERR(j, break)                                 \
 		}                                                                  \
-		PJARR_MEMMOVE(j, PJARR_DATA(j) + 1, PJARR_DATA(j), PJARR_SZ(j)++); \
-		*PJARR_DATA(j) = (value);                                          \
+		JARRP_MEMMOVE(j, JARRP_DATA(j) + 1, JARRP_DATA(j), JARRP_SZ(j)++); \
+		*JARRP_DATA(j) = (value);                                          \
 	} while (0)
 
 #define jarr_foreachi(j, iterator) \
 	for (size_t iterator = 0, _max_elem_##iterator = (j)->size; iterator < _max_elem_##iterator; ++iterator)
 
-#define jarr_start(j, p) PJARR_DATA(j)
-#define jarr_end(j, p)   (PJARR_DATA(j) + PJARR_SZ(j))
+#define jarr_start(j, p) JARRP_DATA(j)
+#define jarr_end(j, p)   (JARRP_DATA(j) + JARRP_SZ(j))
 
 #if JSTR_DEBUG
 #	define jarr_at(j, idx) \
-		(jstr_likely(idx < PJARR_SZ(j)) ? (PJARR_DATA(j) + (idx)) : (jstr_err_exit("Index out of bounds."), PJARR_DATA(j)))
+		(jstr_likely(idx < JARRP_SZ(j)) ? (JARRP_DATA(j) + (idx)) : (jstr_err_exit("Index out of bounds."), JARRP_DATA(j)))
 #else
 #	define jarr_at(j, idx) \
-		(PJARR_DATA(j) + (idx))
+		(JARRP_DATA(j) + (idx))
 #endif
 
 JSTRP_BEGIN_DECLS
@@ -221,7 +221,7 @@ JSTR_MAYBE_UNUSED
 JSTR_NOINLINE
 JSTR_COLD
 static void
-PJARR_ERR(const char *JSTR_RESTRICT FILE_,
+JARRP_ERR(const char *JSTR_RESTRICT FILE_,
           const int LINE_,
           const char *JSTR_RESTRICT func_)
 {
@@ -232,7 +232,7 @@ JSTR_MAYBE_UNUSED
 JSTR_NOINLINE
 JSTR_COLD
 static void
-PJARR_ERR_EXIT(const char *JSTR_RESTRICT FILE_,
+JARRP_ERR_EXIT(const char *JSTR_RESTRICT FILE_,
                const int LINE_,
                const char *JSTR_RESTRICT func_,
                const char *JSTR_RESTRICT msg)
--- src/jstr-config.h
+++ src/jstr-config.h
@@ -3,6 +3,9 @@
 #ifndef JSTR_CONFIG_H
 #define JSTR_CONFIG_H 1
 
+#define JSTR_MIN_CAP ((sizeof(size_t) == 8) ? 24 : 16)
+#define JSTRP_MALLOC_ALIGNMENT (sizeof(size_t) + sizeof(size_t))
+
 /* #define JSTR_DEBUG 1 */
 /* #define JARR_DEBUG 1 */
 /* #define JSTRL_LAZY_FREE 1 */
@@ -19,16 +22,12 @@
 #	define JSTR_ENDIAN_BIG    1
 #endif
 
-#define JSTR_MIN_CAP ((sizeof(size_t) == 8) ? 24 : 16)
-
-#define JSTRP_MALLOC_ALIGNMENT (sizeof(size_t) + sizeof(size_t))
-
-#ifndef PJARR_GROWTH
-#	define PJARR_GROWTH 1.5
+#ifndef JARRP_GROWTH
+#	define JARRP_GROWTH 1.5
 #endif
 
-#ifndef PJARR_ALLOC_MULTIPLIER
-#	define PJARR_ALLOC_MULTIPLIER 1.5
+#ifndef JARRP_ALLOC_MULTIPLIER
+#	define JARRP_ALLOC_MULTIPLIER 1.5
 #endif
 
 #ifndef JSTR_GROWTH
