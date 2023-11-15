cdb823fa5b18693ab43278a514ff19748210bf5f
James Tirta Halim <tirtajames45@gmail.com>
2023-11-15 23:09:07 +0700

    new

--- src/jstr-list.h
+++ src/jstr-list.h
@@ -304,6 +304,13 @@ jstrl_pushfront_len_unsafe(jstrlist_ty *R l,
 	if (jstr_likely(l->size))
 		pjstrl_memmove(l->data + 1, l->data, l->size * sizeof(*l->data));
 	++l->size;
+#ifdef __clang__
+#	pragma GCC diagnostic ignored "-Wunknown-pragmas"
+#	pragma GCC diagnostic push
+#elif defined __GNUC__
+#	pragma GCC diagnostic ignored "-Wanalyzer-null-argument"
+#	pragma GCC diagnostic push
+#endif
 	if (jstr_unlikely(
 	    !pjstrl_assign_len(
 	    &l->data->data,
@@ -312,6 +319,11 @@ jstrl_pushfront_len_unsafe(jstrlist_ty *R l,
 	    s,
 	    s_len)))
 		goto err;
+#ifdef __clang__
+#	pragma GCC diagnostic pop
+#elif defined __GNUC__
+#	pragma GCC diagnostic pop
+#endif
 	return 1;
 err:
 	jstrl_free(l);
