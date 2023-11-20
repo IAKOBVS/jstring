6f5f25a71bb5d0d1e2f335b52bc371f569cf6c05
James Tirta Halim <tirtajames45@gmail.com>
2023-11-20 10:17:41 +0700

    new

--- src/_jstr-features.h
+++ src/_jstr-features.h
@@ -1,9 +1,22 @@
+/* Copyright (C) 1991-2023 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <https://www.gnu.org/licenses/>.  */
+
 #ifndef JSTR_FEATURES_H
 #define JSTR_FEATURES_H
-
-/* The following is based on the features.h from the GNU C Library
-   released under the terms of the GNU Lesser General Public License.
-   Copyright (C) 1991-2023 Free Software Foundation, Inc. */
 
 /* This is to enable the ISO C11 extension.  */
 #if (defined _ISOC11_SOURCE || defined _ISOC2X_SOURCE \
