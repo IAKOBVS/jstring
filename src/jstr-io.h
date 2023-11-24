1a5d511c6244d33aba669fc1cd2110db552b43e4
James Tirta Halim <tirtajames45@gmail.com>
2023-11-24 15:59:15 +0700

    new

--- src/jstr-io.h
+++ src/jstr-io.h
@@ -763,7 +763,7 @@ do_reg:
 			else /* RET_STOP */
 				goto err_closedir;
 		} else {
-			if (jstr_chk(ret))
+			if (ret == JSTRIO_FTW_RET_STOP)
 				goto err_closedir;
 		}
 		continue;
@@ -790,7 +790,7 @@ do_dir:
 			else /* RET_STOP */
 				goto err_closedir;
 		} else {
-			if (jstr_chk(ret))
+			if (ret == JSTRIO_FTW_RET_STOP)
 				goto err_closedir;
 		}
 CONT:
