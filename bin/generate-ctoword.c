80003fd9b9547a94b4875274d46a077bd78939b5
James Tirta Halim <tirtajames45@gmail.com>
2023-11-24 21:05:10 +0700

    new

--- bin/generate-ctoword.c
+++ bin/generate-ctoword.c
@@ -1,8 +1,32 @@
+/* Copyright (c) 2023 James Tirta Halim <tirtajames45 at gmail dot com>
+   This file is part of the jstring library.
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included in all
+   copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+   SOFTWARE.
+
+   MIT License (Expat) */
+
 #include <stdio.h>
 #include <stdlib.h>
 
-void generate(unsigned int i,
-	      const unsigned int j)
+void
+generate(unsigned int i,
+         const unsigned int j)
 {
 	for (; i <= j; ++i) {
 		printf("case %u: return ", i);
@@ -12,7 +36,8 @@ void generate(unsigned int i,
 	}
 }
 
-int main(int argc, char **argv)
+int
+main(int argc, char **argv)
 {
 	if (argc != 3) {
 		fprintf(stderr, "Usage: %s: <min_bytes> <max_bytes>", argv[0]);
