2c2288f4980e751519dbd835655b7c539e497c76
James Tirta Halim <tirtajames45@gmail.com>
2023-11-28 15:52:14 +0700

    new

--- src/jstr-string.h
+++ src/jstr-string.h
@@ -621,7 +621,7 @@ JSTR_NOEXCEPT
 	return jstr_memmem(hs, JSTR_MIN(hs_len, n), ne, ne_len);
 }
 
-#define JSTR_USE_STRRSTR 1
+#define JSTR_USE_STRRSTR 0
 
 #ifdef JSTR_USE_STRRSTR /* Broken. */
 #	define PJSTR_RAREBYTE_RETTYPE char *
