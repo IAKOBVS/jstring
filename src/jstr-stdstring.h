586fbdf8c9b74d0e44008f688a8c0c5feabc1243
James Tirta Halim <tirtajames45@gmail.com>
2023-12-04 14:13:05 +0700

    new

--- src/jstr-stdstring.h
+++ src/jstr-stdstring.h
@@ -197,14 +197,31 @@ JSTR_NOEXCEPT
 {
 #if JSTR_HAVE_MEMRCHR
 	return (void *)memrchr(s, c, n);
-#elif 0
-	enum {
-		PRINT = 1
-	};
-	typedef const unsigned char cu;
+#else
+	/* The following is based on musl's memchr().
+	   Copyright © 2005-2020 Rich Felker, et al.
+
+	   Permission is hereby granted, free of charge, to any person obtaining
+	   a copy of this software and associated documentation files (the
+	   "Software"), to deal in the Software without restriction, including
+	   without limitation the rights to use, copy, modify, merge, publish,
+	   distribute, sublicense, and/or sell copies of the Software, and to
+	   permit persons to whom the Software is furnished to do so, subject to
+	   the following conditions:
+
+	   The above copyright notice and this permission notice shall be
+	   included in all copies or substantial portions of the Software.
+
+	   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+	   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+	   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+	   IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+	   CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+	   TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+	   SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. */
 	if (jstr_unlikely(n == 0))
 		return NULL;
-	cu *p = (cu *)s + n - 1;
+	const unsigned char *p = (const unsigned char *)s + n - 1;
 	c = (unsigned char)c;
 #	if JSTR_HAVE_ATTR_MAY_ALIAS
 #		define SS         (sizeof(size_t))
@@ -212,18 +229,15 @@ JSTR_NOEXCEPT
 #		define ONES       ((size_t)-1 / UCHAR_MAX)
 #		define HIGHS      (ONES * (UCHAR_MAX / 2 + 1))
 #		define HASZERO(x) (((x)-ONES) & ~(x)&HIGHS)
-	for (; ((uintptr_t)p & ALIGN) && n; --p, --n)
-		if (*p == c)
-			return (char *)p;
+	for (; (uintptr_t)(p - 1) & ALIGN && n && *p != c; --n, --p)
+		;
 	if (n >= SS && *p != c) {
 		typedef size_t JSTR_ATTR_MAY_ALIAS word;
+		const word *w = (const word *)(p - SS + 1);
 		const size_t k = ONES * (unsigned char)c;
-		const word *w = w = (const word *)p - 1;
-		for (n += SS; n >= SS; --w, n -= SS)
-			if (HASZERO(*w ^ k))
-				for (p = (cu *)w + SS - 1, n = SS; n--; --p)
-					if (*p == c)
-						return (char *)p;
+		for (; n >= SS && !HASZERO(*w ^ k); --w, n -= SS)
+			;
+		p = (unsigned char *)w + SS - 1;
 	}
 #		undef SS
 #		undef ALIGN
@@ -234,14 +248,6 @@ JSTR_NOEXCEPT
 	for (; n--; --p)
 		if (*p == c)
 			return (char *)p;
-	return NULL;
-#elif JSTR_HAVE_WORD_AT_A_TIME && JSTR_USE_LGPL
-#	include "_lgpl-memrchr.h"
-#else
-	const unsigned char *p = (const unsigned char *)s + n;
-	for (; n--;)
-		if (*--p == (unsigned char)c)
-			return (void *)p;
 	return NULL;
 #endif
 }
