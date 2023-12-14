10abfb418c68a8928b914b68c004e0c1308f03c8
James Tirta Halim <tirtajames45@gmail.com>
2023-12-14 20:25:50 +0700

    new

--- tests/test-string.c
+++ tests/test-string.c
@@ -358,6 +358,7 @@ main(int argc, char **argv)
 	T_CPY(jstr_stpcpy, simple_stpcpy, test_array_memmem);
 
 #ifdef __AVX2__
+	T_CPY(pjstr_stpcpy_avx2, simple_stpcpy, test_array_memmem);
 	T_LEN(pjstr_memmem_avx2, simple_memmem, test_array_memmem);
 	T_LEN(pjstr_strcasestr_len_avx2, simple_strcasestr_len, test_array_memmem);
 #endif
