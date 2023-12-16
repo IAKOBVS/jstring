d1e949699d7b00d8e22ddc22f54ffe6089fd683a
James Tirta Halim <tirtajames45@gmail.com>
2023-12-16 13:11:45 +0700

    new

--- src/_musl.h
+++ src/_musl.h
@@ -166,7 +166,10 @@ JSTR_NOEXCEPT
 #if JSTR_HAVE_ATTR_MAY_ALIAS
 	enum { ALIGN = sizeof(size_t) };
 	typedef size_t JSTR_ATTR_MAY_ALIAS word;
-	if ((uintptr_t)src % ALIGN == (uintptr_t)dst % ALIGN) {
+#	if !JSTR_HAVE_UNALIGNED_ACCESS
+	if ((uintptr_t)src % ALIGN == (uintptr_t)dst % ALIGN)
+#	endif
+	{
 		while (JSTR_PTR_IS_NOT_ALIGNED(dst, ALIGN))
 			if (jstr_unlikely((*dst++ = *src++) == '\0'))
 				return dst;
