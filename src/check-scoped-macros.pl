257fc5690a0175ce1c4b6caad21c96f50d7d8e75
James Tirta Halim <tirtajames45@gmail.com>
2023-12-19 13:54:51 +0700

    new

--- src/check-scoped-macros.pl
+++ src/check-scoped-macros.pl
@@ -5,13 +5,13 @@ use warnings;
 do "./jlib.pl";
 
 my $fname = $ARGV[0];
-my $file_str = jl_file_get_str($fname);
+my $file_str = jl_file_get_str(\$fname);
 my @lines = split(/\n/, $file_str);
 foreach (@lines) {
 	if (/^[ \t]*#[ \t]*define[ \t]*([A-Z][A-Z0-9_]*)/) {
 		my $macro = $1;
-		if ($file_str !~ /^[ \t]*#[ \t]*undef[ \t][ \t]$macro/) {
-			print "$macro is not defined in $fname.\n";
+		if ($file_str !~ /(?:^|\n)[ \t]*#[ \t]*undef[ \t]+$macro/) {
+			print "$macro is not undefined in $fname.\n";
 		}
 	}
 }
