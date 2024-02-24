e51b8cfd07fce2b93adc3615b546a512f05379e0
James Tirta Halim <tirtajames45@gmail.com>
2024-02-24 19:03:36 +0700

    new

--- src/jstr-string.h
+++ src/jstr-string.h
@@ -39,9 +39,6 @@ PJSTR_END_DECLS
 
 #if defined __AVX512BW__ || defined __AVX2__ || defined __SSE2__
 #	define JSTR_HAVE_SIMD 1
-#endif
-
-#if JSTR_HAVE_SIMD
 #	include "_simd.h"
 #endif
 
@@ -354,7 +351,7 @@ JSTR_NOEXCEPT
 #if JSTR_USE_MEMMEM_LIBC
 	return memmem(hs, hs_len, ne, ne_len);
 #elif JSTR_HAVE_SIMD && !JSTR_HAVENT_MEMMEM_SIMD
-	if (jstr_unlikely(ne_len > 256))
+	if (jstr_unlikely(ne_len > sizeof (jstr_vec_ty)))
 		return (hs_len >= ne_len) ? pjstr_memmem_musl((cu *)hs, hs_len, (cu *)ne, ne_len) : NULL;
 	return pjstr_memmem_simd(hs, hs_len, ne, ne_len);
 #else
@@ -409,7 +406,7 @@ jstr_memmem_comp(jstr_twoway_ty *const t,
 JSTR_NOEXCEPT
 {
 #if JSTR_HAVE_SIMD && !JSTR_HAVENT_MEMMEM_SIMD
-	if (jstr_likely(ne_len <= 256)) {
+	if (jstr_likely(ne_len <= sizeof(jstr_vec_ty))) {
 		t->needle_len = ne_len;
 		return;
 	}
@@ -433,7 +430,7 @@ JSTR_NOEXCEPT
 {
 	typedef const unsigned char cu;
 #if JSTR_HAVE_SIMD && !JSTR_HAVENT_MEMMEM_SIMD
-	if (jstr_likely(t->needle_len <= 256))
+	if (jstr_likely(t->needle_len <= sizeof(jstr_vec_ty)))
 		return pjstr_memmem_simd(hs, hs_len, ne, t->needle_len);
 #else
 	if (jstr_unlikely(t->needle_len == 0))
@@ -706,9 +703,9 @@ JSTR_NOEXCEPT
 	}
 	typedef const unsigned char cu;
 #if JSTR_HAVE_SIMD && !JSTR_HAVENT_STRCASESTR_LEN_SIMD
-	if (jstr_unlikely(ne_len > 256))
+	if (jstr_unlikely(ne_len > sizeof(jstr_vec_ty)))
 		return (hs_len >= ne_len) ? pjstr_strcasestr_len_musl((cu *)hs, hs_len, (cu *)ne, ne_len) : NULL;
-	return pjstr_strcasestr_len_simd(hs, hs_len, ne, ne_len);
+	return (char *)pjstr_strcasestr_len_simd(hs, hs_len, ne, ne_len);
 #else
 	if (ne_len == 1)
 		return (char *)jstr_memcasechr(hs, *ne, hs_len);
@@ -761,7 +758,7 @@ jstr_strcasestr_len_comp(jstr_twoway_ty *const t,
 JSTR_NOEXCEPT
 {
 #if JSTR_HAVE_SIMD && !JSTR_HAVENT_STRCASESTR_LEN_SIMD
-	if (jstr_likely(ne_len <= 256)) {
+	if (jstr_likely(ne_len <= sizeof(jstr_vec_ty))) {
 		t->needle_len = ne_len;
 		return;
 	}
@@ -785,7 +782,7 @@ JSTR_NOEXCEPT
 {
 	typedef const unsigned char cu;
 #if JSTR_HAVE_SIMD && !JSTR_HAVENT_STRCASESTR_LEN_SIMD
-	if (jstr_likely(t->needle_len <= 256))
+	if (jstr_likely(t->needle_len <= sizeof(jstr_vec_ty)))
 		return pjstr_strcasestr_len_simd(hs, hs_len, ne, t->needle_len);
 #else
 	if (jstr_unlikely(t->needle_len == 0))
