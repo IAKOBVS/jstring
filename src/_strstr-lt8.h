7b56f984c2539d6bad6aac986509e197918f1019
James Tirta Halim <tirtajames45@gmail.com>
2024-03-18 21:34:42 +0700

    new

--- src/_strstr-lt8.h
+++ src/_strstr-lt8.h
@@ -58,10 +58,8 @@ static char *
 JSTR_CONCAT(JSTR__STRSTR234_FUNC_NAME, _lt8)(const unsigned char *hs, size_t hs_len, const unsigned char *ne, unsigned int ne_len)
 JSTR_NOEXCEPT
 {
-	uint64_t hw;
-	uint64_t nw;
-	const uint64_t mask
-	= (uint64_t)-1 >> ((sizeof(uint64_t) - ne_len) << 3);
+	uint64_t hw, nw;
+	const uint64_t mask = (uint64_t)-1 >> ((sizeof(uint64_t) - ne_len) << 3);
 	for (unsigned int n = ne_len; n--; hw = (uint64_t)hw << 8 | L(*hs++), nw = (uint64_t)nw << 8 | L(*ne++))
 		N_EXIT;
 	for (hs_len -= ne_len, hw &= mask, nw &= mask; N hw != nw && hs_len--; hw = (uint64_t)(hw << 8 | L(*hs++)) & mask) {}
@@ -89,10 +87,8 @@ static char *
 JSTR_CONCAT(JSTR__STRSTR234_FUNC_NAME, _lt8)(const unsigned char *hs, size_t hs_len, const unsigned char *ne, unsigned int ne_len)
 JSTR_NOEXCEPT
 {
-	uint64_t hw;
-	uint64_t nw;
-	const uint64_t mask
-	= (uint64_t)-1 >> ((sizeof(uint64_t) - ne_len) << 3);
+	uint64_t hw, nw;
+	const uint64_t mask = (uint64_t)-1 >> ((sizeof(uint64_t) - ne_len) << 3);
 	hs += hs_len - 1;
 	ne += ne_len - 1;
 	for (unsigned int n = ne_len; n--; hw = (uint64_t)hw << 8 | L(*hs--), nw = (uint64_t)nw << 8 | L(*ne--))
@@ -130,10 +126,8 @@ static char *
 JSTR_CONCAT(JSTR__STRSTR234_FUNC_NAME, _lt8)(const unsigned char *hs, const unsigned char *ne, unsigned int ne_len)
 JSTR_NOEXCEPT
 {
-	uint64_t hw;
-	uint64_t nw;
-	const uint64_t mask
-	= (uint64_t)-1 >> ((sizeof(uint64_t) - ne_len) << 3);
+	uint64_t hw, nw;
+	const uint64_t mask = (uint64_t)-1 >> ((sizeof(uint64_t) - ne_len) << 3);
 	for (; *ne; hw = (uint64_t)hw << 8 | L(*hs++), nw = (uint64_t)nw << 8 | L(*ne++)) {
 		N_EXIT;
 		if (jstr_unlikely(*hs == '\0'))
