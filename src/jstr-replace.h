6ed68ec1511fdb84e7668af942e5c8f1f6edcf67
James Tirta Halim <tirtajames45@gmail.com>
2024-03-18 12:37:43 +0700

    new

--- src/jstr-replace.h
+++ src/jstr-replace.h
@@ -916,8 +916,7 @@ loop1:
 			i.src = (const char *)alloca(*sz);
 			/* Copy the original string to SRC. */
 			memcpy((char *)i.src, *s, *sz);
-			/* Update the pointers to point to SRC
-			 * since we're modifying DST. */
+			/* Update the pointers to point to SRC since we're modifying DST. */
 			first = (char *)i.src + (first - *s);
 			last = i.src + (last - *s);
 			end = i.src + (end - *s);
