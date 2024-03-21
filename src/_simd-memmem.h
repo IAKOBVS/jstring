02a8d9f064c117377fd29dc323b1ac9d99f147af
James Tirta Halim <tirtajames45@gmail.com>
2024-03-21 23:28:27 +0700

    remove simd_strcasestr

--- src/_simd-memmem.h
+++ src/_simd-memmem.h
@@ -64,50 +64,23 @@ JSTR__END_DECLS
 #ifndef JSTR__SIMD_MEMMEM_CMP_FUNC
 #	define JSTR__SIMD_MEMMEM_CMP_FUNC memcmp
 #endif
-#ifndef JSTR__SIMD_MEMMEM_MEMCASECHR
-#	define JSTR__SIMD_MEMMEM_MEMCASECHR jstr__simd_memcasechr
-#endif
 
-#ifdef JSTR__SIMD_MEMMEM_USE_AS_ICASE
-#	define OR_UPPER_MASK(x) | (x)
-#else
-#	define OR_UPPER_MASK(x)
-#endif
-
-#ifndef JSTR__SIMD_MEMMEM_USE_AS_ICASE
-#	define FIND_MATCH()                                                 \
-		do {                                                         \
-			if (JSTR_PTR_NOT_CROSSING_PAGE(                      \
-			    p,                                               \
-			    VEC_SIZE,                                        \
-			    JSTR_PAGE_SIZE)) {                               \
-				hv = LOADU((const VEC *)p);                  \
-				cmpm = (MASK)CMPEQ8_MASK(hv, nv) << matchsh; \
-				if (cmpm == matchm)                          \
-					return (JSTR__SIMD_RETTYPE)p;        \
-			} else {                                             \
-				if (!JSTR__SIMD_MEMMEM_CMP_FUNC(             \
-				    (const char *)p,                         \
-				    (const char *)ne,                        \
-				    ne_len))                                 \
-					return (JSTR__SIMD_RETTYPE)p;        \
-			}                                                    \
-		} while (0)
-#else
-#	define FIND_MATCH()                                  \
-		do {                                          \
-			if (!JSTR__SIMD_MEMMEM_CMP_FUNC(      \
-			    (const char *)p,                  \
-			    (const char *)ne,                 \
-			    ne_len))                          \
-				return (JSTR__SIMD_RETTYPE)p; \
-		} while (0)
-#endif
+#define FIND_MATCH()                                                                                \
+	do {                                                                                        \
+		if (JSTR_PTR_NOT_CROSSING_PAGE(p, VEC_SIZE, JSTR_PAGE_SIZE)) {                      \
+			hv = LOADU((const VEC *)p);                                                 \
+			cmpm = (MASK)CMPEQ8_MASK(hv, nv) << matchsh;                                \
+			if (cmpm == matchm)                                                         \
+				return (ret_ty)p;                                                   \
+		} else {                                                                            \
+			if (!JSTR__SIMD_MEMMEM_CMP_FUNC((const char *)p, (const char *)ne, ne_len)) \
+				return (ret_ty)p;                                                   \
+		}                                                                                   \
+	} while (0)
 
 JSTR__BEGIN_DECLS
 
 /* ne_len must be <= VEC_SIZE */
-
 JSTR_ATTR_ACCESS((__read_only__, 1, 2))
 JSTR_ATTR_ACCESS((__read_only__, 3, 4))
 JSTR_FUNC_PURE
@@ -116,25 +89,17 @@ static JSTR__SIMD_RETTYPE
 JSTR__SIMD_MEMMEM_FUNC_NAME(const void *hs, size_t hs_len, const void *ne, size_t ne_len)
 JSTR_NOEXCEPT
 {
+	typedef JSTR__SIMD_RETTYPE ret_ty;
 	if (ne_len == 1)
-#ifndef JSTR__SIMD_MEMMEM_USE_AS_ICASE
-		return (JSTR__SIMD_RETTYPE)memchr(hs, *(unsigned char *)ne, hs_len);
-#else
-		return (JSTR__SIMD_RETTYPE)
-		JSTR__SIMD_MEMMEM_MEMCASECHR(hs, *(unsigned char *)ne, hs_len);
-#endif
+		return (ret_ty)memchr(hs, *(unsigned char *)ne, hs_len);
 	if (jstr_unlikely(ne_len == 0))
-		return (JSTR__SIMD_RETTYPE)hs;
+		return (ret_ty)hs;
 	if (jstr_unlikely(hs_len < ne_len))
 		return NULL;
 	VEC hv0, hv1;
 	MASK i, hm0, hm1, m;
-#ifndef JSTR__SIMD_MEMMEM_USE_AS_ICASE
 	VEC hv, nv;
 	MASK cmpm;
-#else
-	MASK hm0u, hm1u;
-#endif
 	const unsigned char *h = (const unsigned char *)hs;
 	const unsigned char *const end = h + hs_len - ne_len;
 	/* Find a unique character pair in NE. For example, "ab" in
@@ -145,7 +110,6 @@ JSTR_NOEXCEPT
 	/* N must be the "ab", not the "bb". */
 	n = JSTR_PTR_DIFF(p, ne) - 1;
 	h += n;
-#ifndef JSTR__SIMD_MEMMEM_USE_AS_ICASE
 	const unsigned int matchsh = ne_len < VEC_SIZE ? VEC_SIZE - ne_len : 0;
 	const MASK matchm = ONES << matchsh;
 	const VEC nv0 = SETONE8(*((char *)ne + n));
@@ -154,31 +118,14 @@ JSTR_NOEXCEPT
 		nv = LOADU((const VEC *)ne);
 	else
 		memcpy(&nv, ne, ne_len);
-#else
-	const VEC nv0 = SETONE8((char)jstr_tolower(*((unsigned char *)ne + n)));
-	const VEC nv1
-	= SETONE8((char)jstr_tolower(*((unsigned char *)ne + n + 1)));
-	const VEC nv0u
-	= SETONE8((char)jstr_toupper(*((unsigned char *)ne + n)));
-	const VEC nv1u
-	= SETONE8((char)jstr_toupper(*((unsigned char *)ne + n + 1)));
-#endif
-	const unsigned int off_s
-	= JSTR_PTR_DIFF(h, JSTR_PTR_ALIGN_DOWN(h, VEC_SIZE));
-	unsigned int off_e = (JSTR_PTR_DIFF(end, (h - n)) < VEC_SIZE)
-	                     ? VEC_SIZE - (unsigned int)(end - (h - n)) - 1
-	                     : 0;
+	const unsigned int off_s = JSTR_PTR_DIFF(h, JSTR_PTR_ALIGN_DOWN(h, VEC_SIZE));
+	unsigned int off_e = (JSTR_PTR_DIFF(end, (h - n)) < VEC_SIZE) ? VEC_SIZE - (unsigned int)(end - (h - n)) - 1 : 0;
 	h -= off_s;
 	hv0 = LOAD((const VEC *)h);
 	hm0 = (MASK)CMPEQ8_MASK(hv0, nv0);
 	hm1 = (MASK)CMPEQ8_MASK(hv0, nv1) >> 1;
-#ifdef JSTR__SIMD_MEMMEM_USE_AS_ICASE
-	hm0u = (MASK)CMPEQ8_MASK(hv0, nv0u);
-	hm1u = (MASK)CMPEQ8_MASK(hv0, nv1u) >> 1;
-#endif
 	/* Clear matched bits that are out of bounds. */
-	m = (((hm0 OR_UPPER_MASK(hm0u)) & (hm1 OR_UPPER_MASK(hm1u))) >> off_s)
-	    & (ONES >> off_e);
+	m = (hm0 & hm1 >> off_s) & (ONES >> off_e);
 	while (m) {
 		i = TZCNT(m);
 		m = BLSR(m);
@@ -191,11 +138,7 @@ JSTR_NOEXCEPT
 		hv1 = LOAD((const VEC *)(h + 1));
 		hm0 = (MASK)CMPEQ8_MASK(hv0, nv0);
 		hm1 = (MASK)CMPEQ8_MASK(hv1, nv1);
-#ifdef JSTR__SIMD_MEMMEM_USE_AS_ICASE
-		hm0u = (MASK)CMPEQ8_MASK(hv0, nv0u);
-		hm1u = (MASK)CMPEQ8_MASK(hv1, nv1u);
-#endif
-		m = (hm0 OR_UPPER_MASK(hm0u)) & (hm1 OR_UPPER_MASK(hm1u));
+		m = hm0 & hm1;
 		while (m) {
 match:
 			i = TZCNT(m);
@@ -210,12 +153,7 @@ match:
 		hv1 = LOAD((const VEC *)(h + 1));
 		hm0 = (MASK)CMPEQ8_MASK(hv0, nv0);
 		hm1 = (MASK)CMPEQ8_MASK(hv1, nv1);
-#ifdef JSTR__SIMD_MEMMEM_USE_AS_ICASE
-		hm0u = (MASK)CMPEQ8_MASK(hv0, nv0u);
-		hm1u = (MASK)CMPEQ8_MASK(hv1, nv1u);
-#endif
-		m = (hm0 OR_UPPER_MASK(hm0u)) & (hm1 OR_UPPER_MASK(hm1u))
-		    & (ONES >> off_e);
+		m = hm0 & hm1 & (ONES >> off_e);
 		if (m)
 			goto match;
 	}
@@ -239,8 +177,5 @@ JSTR__END_DECLS
 
 #undef JSTR__SIMD_MEMMEM_FUNC_NAME
 #undef JSTR__SIMD_MEMMEM_CMP_FUNC
-#undef JSTR__SIMD_MEMMEM_MEMCASECHR
-#undef JSTR__SIMD_MEMMEM_USE_AS_ICASE
 #undef JSTR__SIMD_RETTYPE
-#undef OR_UPPER_MASK
 #undef FIND_MATCH
