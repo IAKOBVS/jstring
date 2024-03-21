b04a2c0dbcd47416c05deb1103e18759ff1140e8
James Tirta Halim <tirtajames45@gmail.com>
2024-03-21 14:51:50 +0700

    fix re_rplcn

--- src/jstr-macros.h
+++ src/jstr-macros.h
@@ -164,7 +164,7 @@ JSTR__END_DECLS
 
 #define jstr_chk(ret)             jstr_unlikely(ret == -1)
 #define jstr_nullchk(p)           jstr_unlikely((p) == NULL)
-#define JSTR_PAGE_SIZE 4096
+#define JSTR_PAGE_SIZE            4096
 #define JSTR_ARRAY_COUNT(array)   (sizeof(array) / sizeof(array[0]))
 #define JSTR__CONCAT_HELPER(x, y) x##y
 #define JSTR_CONCAT(x, y)         JSTR__CONCAT_HELPER(x, y)
@@ -485,9 +485,20 @@ case '~':
 				assert(expr);         \
 			}                             \
 		} while (0)
+#	define JSTR_ASSERT_DEBUG_PRINTF(expr, ...)   \
+		do {                                  \
+			if (jstr_unlikely(!(expr))) { \
+				jstr_err("");         \
+				fprintf(stderr, __VA_ARGS__); \
+				assert(expr);         \
+			}                             \
+		} while (0)
 #else
 #	define JSTR_ASSERT_DEBUG(expr, msg) \
 		do {                         \
+		} while (0)
+#	define JSTR_ASSERT_DEBUG_PRINTF(expr, ...) \
+		do {                                \
 		} while (0)
 #endif
 
